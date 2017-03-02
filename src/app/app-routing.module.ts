2fc33ccdae1c6d48f9a73d8fff498224a4804b37
kplese <kresimir.plese@gmail.com>
2017-03-02 17:36:33 +0100

    Adding routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AgencyAdminComponent} from "./agency-admin/agency-admin.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login',  component: LoginComponent },
+  { path: 'agency-admin',  component: AgencyAdminComponent }
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2'
-
 import { AppComponent } from './app.component';
-import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 import { LoginComponent } from './login/login.component';
 import { SystemAdminComponent } from './system-admin/system-admin.component';
 import { AgencyAdminComponent } from './agency-admin/agency-admin.component';
@@ -18,6 +16,8 @@ import { CountryOfficeProfileComponent } from './country-office-profile/country-
 import { MapComponent } from './map/map.component';
 import { DirectorDashboardComponent } from './director-dashboard/director-dashboard.component';
 import { DonorModuleComponent } from './donor-module/donor-module.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 // Must export the config
 export const firebaseConfig = {
@@ -36,7 +36,6 @@ const firebaseAuthConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent,
     LoginComponent,
     SystemAdminComponent,
     AgencyAdminComponent,
@@ -54,6 +53,7 @@ const firebaseAuthConfig = {
     BrowserModule,
     FormsModule,
     HttpModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   providers: [],
