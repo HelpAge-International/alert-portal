3e635d1e99cf72af59ebc1ed77876f96299cb24f
Fei Xie <fei@rolleragency.co.uk>
2017-03-09 17:33:04 +0000

    system setting now load data and show from firebase

--- src/app/model/system.model.ts
+++ src/app/model/system.model.ts
@@ -2,5 +2,12 @@
  * Created by Fei on 08/03/2017.
  */
 export class ModelSystem {
-  public fileSize:number;
+  public fileSize: number;
+  public fileType: number;
+  public assignHazard: number;
+  public advThreshold: number[];
+  public fileSettings: boolean[];
+  public genericAction: any;
+  public minThreshold: number[];
 }
+
--- src/app/system-admin/system-settings/system-settings.component.ts
+++ src/app/system-admin/system-settings/system-settings.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth, AngularFire} from "angularfire2";
-import {Constants} from "../../utils/Constants";
+import {Constants, FILE_SETTING} from "../../utils/Constants";
+import {ModelSystem} from "../../model/system.model";
 
 @Component({
   selector: 'app-system-settings',
@@ -15,20 +16,47 @@ export class SystemSettingsComponent implements OnInit {
   advGreen: number;
   advAmber: number;
   advRed: number;
-  isPdf:boolean;
-  isHtml:boolean;
-  isDoc:boolean;
-  isDocx:boolean;
-  isPs:boolean;
-  isRtf:boolean;
-  isJpeg:boolean;
-  isPng:boolean;
-  fileSize:number;
+  isPdf: boolean;
+  isHtml: boolean;
+  isDoc: boolean;
+  isDocx: boolean;
+  isPs: boolean;
+  isRtf: boolean;
+  isJpeg: boolean;
+  isPng: boolean;
+  fileSize: number;
+  fileType: number;
+  thresholdValue: number[] = Constants.THRESHOLD_VALUE;
+  fileTypeList: number[] = [0, 1, 2];
+  fileTypeListName: string[] = ["KB", "MB", "GB"];
 
   constructor(private af: AngularFire) {
   }
 
   ngOnInit() {
-    this.af.database.object(Constants.APP_STATUS+"/system/hoXTsvefEranzaSQTWbkhpBenLn2");
+    this.af.database.object(Constants.APP_STATUS + "/system/hoXTsvefEranzaSQTWbkhpBenLn2").subscribe(x => {
+      console.log(x)
+      // console.log(x.fileSettings[FILE_SETTING.PNG])
+      //load minimum threshold from database
+      this.minGreen = x.minThreshold[0];
+      this.minAmber = x.minThreshold[1];
+      this.minRed = x.minThreshold[2];
+      //load advanced threshold from database
+      this.advGreen = x.advThreshold[0];
+      this.advAmber = x.advThreshold[1];
+      this.advRed = x.advThreshold[2];
+      //load file setting from database
+      this.isPdf = x.fileSettings[FILE_SETTING.PDF];
+      this.isHtml = x.fileSettings[FILE_SETTING.HTML];
+      this.isDoc = x.fileSettings[FILE_SETTING.DOC];
+      this.isDocx = x.fileSettings[FILE_SETTING.DOCX];
+      this.isPs = x.fileSettings[FILE_SETTING.PS];
+      this.isRtf = x.fileSettings[FILE_SETTING.RTF];
+      this.isJpeg = x.fileSettings[FILE_SETTING.JPEG];
+      this.isPng = x.fileSettings[FILE_SETTING.PNG];
+      //load file size type
+      this.fileSize = x.fileSize;
+      this.fileType = x.fileType;
+    });
   }
 }
