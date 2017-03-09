d66e32f9d6ceddaa900f018d22f1fb2295365f9a
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-03-09 17:17:05 +0000

    Editing CHS actions

--- src/app/system-admin/min-prep/create-action/create-action.component.ts
+++ src/app/system-admin/min-prep/create-action/create-action.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {AngularFire} from "angularfire2";
-import {Router} from "@angular/router";
-import {ChsMinPreparednessAction} from '../../../model/chsMinPreparednessAction';
-import {Constants} from '../../../utils/Constants';
-import {ActionType} from "../../../utils/Enums";
+import { Component, OnInit } from '@angular/core';
+import { AngularFire } from "angularfire2";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { ChsMinPreparednessAction } from '../../../model/chsMinPreparednessAction';
+import { Constants } from '../../../utils/Constants';
+import { ActionType } from "../../../utils/Enums";
 
 @Component({
   selector: 'app-create-action',
@@ -14,31 +14,71 @@ export class CreateActionComponent implements OnInit {
 
   private inactive: Boolean = true;
   private errorMessage: any;
+  private pageTitle: string = 'Create new CHS minimum preparedness action';
+  private buttonText: string = 'Save new action';
   private textArea: string;
-  private newAction: ChsMinPreparednessAction;
   private path: string = Constants.APP_STATUS + "/action/" + Constants.uid;
+  private forEditing = false;
+  private idOfChsActionToEdit: string;
 
-  constructor(private af: AngularFire, private router: Router) {
+  constructor(private af: AngularFire, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params: Params) => {
+        if (params["id"]) {
+          this.forEditing = true;
+          this.pageTitle = "Edit CHS minimum preparedness action";
+          this.buttonText = "Edit action";
+          this.loadCHSActionInfo(params["id"]);
+          this.idOfChsActionToEdit = params["id"];
+        }
+      });
   }
 
-  saveNewAction() {
+  private loadCHSActionInfo(actionId: string) {
+    console.log(actionId);
+    console.log(this.path+actionId);
+    this.af.database.object(this.path+ '/' + actionId).subscribe((action:ChsMinPreparednessAction) => {
+      this.textArea = action.task;
+    });
+  }
+
+  onSubmit() {
+
     if (this.validate()) {
 
-      this.newAction = new ChsMinPreparednessAction(this.textArea, ActionType.chs);
-      this.af.database.list(this.path).push(this.newAction)
-        .then(_ => {
-          console.log('New CHS action added');
-          this.router.navigateByUrl("/system-admin/min-prep");
-        }
-    );
-      this.inactive = true;
-
+      if (this.forEditing) {
+        //TODO Update Info
+        this.editChsAction();
+      } else {
+        this.addNewChsAction();
+        this.inactive = true;
+      }
     } else {
       this.inactive = false;
     }
+  }
+
+  private addNewChsAction() {
+
+    var newAction: ChsMinPreparednessAction = new ChsMinPreparednessAction(this.textArea, ActionType.chs);
+    this.af.database.list(this.path).push(newAction)
+      .then(_ => {
+          console.log('New CHS action added');
+          this.router.navigateByUrl("/system-admin/min-prep");
+        }
+      );
+  }
+
+  private editChsAction() {
+    var editedAction: ChsMinPreparednessAction = new ChsMinPreparednessAction(this.textArea, ActionType.chs);
+    this.af.database.object(this.path+ "/" + this.idOfChsActionToEdit).set(editedAction).then(_ => {
+        console.log('CHS action updated');
+        this.router.navigateByUrl("/system-admin/min-prep");
+      }
+    );
   }
 
   /**
@@ -46,7 +86,7 @@ export class CreateActionComponent implements OnInit {
    * if no input is entered
    * @returns {boolean}
    */
-  validate() {
+  private validate() {
 
     if (!Boolean(this.textArea)) {
       this.errorMessage = "Please insert the action to create a new CHS minimum preparedness action";
