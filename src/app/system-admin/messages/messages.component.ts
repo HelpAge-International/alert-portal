eaee510e84b210b0c1626a3cb1063cbcd3ed14f2
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-03-22 16:14:32 +0000

    System admin messages deletion refactoring to support new firebase structure

--- src/app/system-admin/messages/messages.component.ts
+++ src/app/system-admin/messages/messages.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit, OnDestroy} from "@angular/core";
-import {AngularFire, FirebaseObjectObservable} from "angularfire2";
-import {Router} from "@angular/router";
-import {Constants} from "../../utils/Constants";
-import {DialogService} from "../../dialog/dialog.service";
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import {Router} from '@angular/router';
+import {Constants} from '../../utils/Constants';
+import {DialogService} from '../../dialog/dialog.service';
 import Promise = firebase.Promise;
-import {Observable} from "rxjs";
-import {RxHelper} from "../../utils/RxHelper";
+import {Observable} from 'rxjs';
+import {RxHelper} from '../../utils/RxHelper';
 
 @Component({
   selector: 'app-messages',
@@ -30,7 +30,7 @@ export class MessagesComponent implements OnInit, OnDestroy {
       if (auth) {
         this.uid = auth.uid;
 
-        let subscription = this.af.database.list(Constants.APP_STATUS + "/systemAdmin/" + this.uid + "/sentmessages")
+        let subscription = this.af.database.list(Constants.APP_STATUS + '/systemAdmin/' + this.uid + '/sentmessages')
           .flatMap(list => {
             this.sentMessages = [];
             let tempList = [];
@@ -40,15 +40,15 @@ export class MessagesComponent implements OnInit, OnDestroy {
             return Observable.from(tempList)
           })
           .flatMap(item => {
-            return this.af.database.object(Constants.APP_STATUS + "/message/" + item.$key)
+            return this.af.database.object(Constants.APP_STATUS + '/message/' + item.$key)
           }).distinctUntilChanged()
           .subscribe(x => {
-              this.sentMessages.push(x);
+            this.sentMessages.push(x);
           });
         this.subscriptions.add(subscription);
       } else {
         // user is not logged in
-        console.log("Error occurred - User is not logged in");
+        console.log('Error occurred - User is not logged in');
         this.navigateToLogin();
       }
     });
@@ -61,41 +61,54 @@ export class MessagesComponent implements OnInit, OnDestroy {
 
   deleteMessage(sentMessage) {
 
-    this.dialogService.createDialog("DELETE_MESSAGE_DIALOG.TITLE", "DELETE_MESSAGE_DIALOG.CONTENT").subscribe(result => {
+    this.dialogService.createDialog('DELETE_MESSAGE_DIALOG.TITLE', 'DELETE_MESSAGE_DIALOG.CONTENT').subscribe(result => {
 
       if (result) {
 
         let key: string = sentMessage.$key;
 
-        let allUsersGroupPath: string = "/messageRef/allusergroup/";
-        let agencyGroupPath: string = Constants.APP_STATUS + "/group/agencygroup/";
-        let countryGroupPath: string = Constants.APP_STATUS + "/group/countrygroup/";
+        this.msgData['/systemAdmin/' + this.uid + '/sentmessages/' + key] = null;
+        this.msgData['/message/' + key] = null;
 
-        this.msgData["/systemAdmin/" + this.uid + "/sentmessages/" + key] = null;
-        this.msgData["/message/" + key] = null;
-        this.msgData[allUsersGroupPath + key] = null;
+        let allUsersGroupPath: string = Constants.APP_STATUS + '/group/systemadmin/allusersgroup';
+        let allAgencyAdminsGroupPath: string = Constants.APP_STATUS + '/group/systemadmin/allagencyadminsgroup';
+        let allCountryAdminsGroupPath: string = Constants.APP_STATUS + '/group/systemadmin/allcountryadminsgroup';
 
-        let subscription = this.af.database.list(agencyGroupPath)
-          .do(list =>{
+        let allUsersMsgRefPath: string = '/messageRef/systemadmin/allusersgroup/';
+        let allAgencyAdminsMsgRef: string = '/messageRef/systemadmin/allagencyadminsgroup/';
+        let allCountryAdminsMsgRef: string = '/messageRef/systemadmin/allcountryadminsgroup/';
+
+        console.log(allUsersGroupPath);
+        let subscription = this.af.database.list(allUsersGroupPath)
+          .do(list => {
+            console.log(list);
             list.forEach(item => {
-              this.msgData["/messageRef/agencygroup/"+item.$key+"/"+key]=null;
-              console.log("item key: "+item.$key);
+              console.log(item.$key);
+              this.msgData[allUsersMsgRefPath + item.$key + '/' + key] = null;
+              console.log(allUsersMsgRefPath + item.$key + '/' + key);
             })
           })
           .subscribe(() => {
-            this.af.database.list(countryGroupPath)
-              .do(list =>{
+            this.af.database.list(allAgencyAdminsGroupPath)
+              .do(list => {
                 list.forEach(item => {
-                  this.msgData["/messageRef/countrygroup/"+item.$key+"/"+key]=null;
-                  console.log("item key: "+item.$key);
+                  this.msgData[allAgencyAdminsMsgRef + item.$key + '/' + key] = null;
                 })
-              }).subscribe(() => {
-              this.af.database.object(Constants.APP_STATUS).update(this.msgData);
-              console.log("Message Ref successfully deleted from all nodes");
-            })
-          })
+              })
+              .subscribe(() => {
+                this.af.database.list(allCountryAdminsGroupPath)
+                  .do(list => {
+                    list.forEach(item => {
+                      this.msgData[allCountryAdminsMsgRef + item.$key + '/' + key] = null;
+                    })
+                  })
+                  .subscribe(() => {
+                    this.af.database.object(Constants.APP_STATUS).update(this.msgData);
+                    console.log('Message Ref successfully deleted from all nodes');
+                  })
+              })
+          });
         this.subscriptions.add(subscription);
-
       }
     });
 
