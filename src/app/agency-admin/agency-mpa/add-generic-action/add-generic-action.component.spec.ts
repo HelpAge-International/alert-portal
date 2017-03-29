c11446ad587b21c867631a9f56eb354afe8a2281
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-03-29 10:31:33 +0100

    With previous comment

---
+++ src/app/agency-admin/agency-mpa/add-generic-action/add-generic-action.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddGenericActionComponent } from './add-generic-action.component';
+
+describe('AddGenericActionComponent', () => {
+  let component: AddGenericActionComponent;
+  let fixture: ComponentFixture<AddGenericActionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddGenericActionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddGenericActionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/agency-admin/agency-mpa/add-generic-action/add-generic-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-generic-action',
+  templateUrl: './add-generic-action.component.html',
+  styleUrls: ['./add-generic-action.component.css']
+})
+export class AddGenericActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
