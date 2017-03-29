8d520e47351fae7918a70b680a209e4425713085
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-03-29 15:32:38 +0100

    WIp agency admin

--- src/app/agency-admin/agency-mpa/add-generic-action/add-generic-action.component.ts
+++ src/app/agency-admin/agency-mpa/add-generic-action/add-generic-action.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {AngularFire, FirebaseListObservable} from "angularfire2";
+import {Router} from "@angular/router";
 import {Constants} from "../../../utils/Constants";
 import {Department, ActionLevel, ActionType, GenericActionCategory} from "../../../utils/Enums";
-import {Router} from "@angular/router";
+import {MandatedPreparednessAction} from "../../../model/mandatedPA";
 import {RxHelper} from "../../../utils/RxHelper";
 import {Observable} from "rxjs";
 
@@ -19,11 +20,13 @@ export class AddGenericActionComponent implements OnInit, OnDestroy {
   private departments: FirebaseListObservable<any>;
   private Department = Department;
   private ActionLevel = ActionLevel;
-  GenericActionCategory = GenericActionCategory;
+  private GenericActionCategory = GenericActionCategory;
+  private actionSelected: boolean;
   private departmentSelected;
   private actionLevelSelected = 0;
   private ActionPrepLevel = Constants.ACTION_LEVEL;
   private levelsList = [ActionLevel.ALL, ActionLevel.MPA, ActionLevel.APA];
+  private selectedActions: MandatedPreparednessAction[] = [];
   private subscriptions: RxHelper;
 
   constructor(private af: AngularFire, private router: Router) {
@@ -50,12 +53,12 @@ export class AddGenericActionComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscriptions.releaseAll();
+    this.selectedActions = [];
   }
 
   filterByLevel() {
 
     console.log("Action level selected - " + this.actionLevelSelected);
-
     if (this.actionLevelSelected == ActionLevel.ALL) {
       this.genericActions = this.af.database.list(Constants.APP_STATUS + "/action/" + Constants.SYSTEM_ADMIN_UID, {
         query: {
@@ -81,6 +84,39 @@ export class AddGenericActionComponent implements OnInit, OnDestroy {
           return tempList;
         });
     }
+  }
+
+  updateSelectedActions(genericAction) {
+
+    let newMandatePA: MandatedPreparednessAction = new MandatedPreparednessAction(genericAction.task, ActionType.mandated, this.departmentSelected, genericAction.level);
+
+    console.log('actionSelected ---- ' + this.actionSelected);
+
+    if (this.actionSelected) {
+
+      var index = this.selectedActions.indexOf(newMandatePA);
+      console.log('index ----' + index);
+
+      if (index < 0) {
+        this.selectedActions.push(newMandatePA);
+        console.log("Adding");
+      } else {
+        this.selectedActions[index] = newMandatePA;
+        console.log("Not added coz index is ---- " + index);
+      }
+
+    } else {
+
+      var index = this.selectedActions.indexOf(newMandatePA);
+
+      if (index > -1) {
+        this.selectedActions.splice(index, 1);
+      }
+    }
+    console.log('selectedActionsCount ---- ' + this.selectedActions.length);
+    console.log('selectedActions ---- ' + this.selectedActions);
+
+    this.actionSelected = null;
   }
 
   private getDepartments() {
