c301ffa07673dead54b9d01adaabf4390a1db1d7
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-04-12 11:23:17 +0100

    WIP new agency details setup page

--- src/app/agency-admin/new-agency/new-agency-details/new-agency-details.component.ts
+++ src/app/agency-admin/new-agency/new-agency-details/new-agency-details.component.ts
@@ -1,15 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {AngularFire} from "angularfire2";
+import {Router} from "@angular/router";
+import {Constants} from "../../../utils/Constants";
+import {Country, Currency} from "../../../utils/Enums";
+import {RxHelper} from "../../../utils/RxHelper";
+import {Observable} from "rxjs";
+import {ModelAgency} from "../../../model/agency.model";
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-new-agency-details',
   templateUrl: './new-agency-details.component.html',
   styleUrls: ['./new-agency-details.component.css']
 })
-export class NewAgencyDetailsComponent implements OnInit {
 
-  constructor() { }
+export class NewAgencyDetailsComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {
+  private uid: string;
+  private agencyAdminName: string;
+
+  private successInactive: boolean = true;
+  private successMessage: string = 'GLOBAL.ACCOUNT_SETTINGS.SUCCESS_PROFILE';
+  private errorInactive: boolean = true;
+  private errorMessage: string;
+  private alerts = {};
+
+  private agencyAddressLine1: string;
+  private agencyAddressLine2: string;
+  private agencyAddressLine3: string;
+  private agencyCity: string;
+  private agencyPostCode: string;
+  private agencyPhone: string;
+  private agencyWebAddress: string;
+  private agencyCountry: number;
+  private agencyCurrency: number;
+
+  private Country = Constants.COUNTRY;
+  private countriesList: number[] = [Country.UK, Country.France, Country.Germany];
+  private Currency = Constants.CURRENCY;
+  private currenciesList: number[] = [Currency.GBP, Currency.USD, Currency.EUR];
+
+  constructor(private af: AngularFire, private router: Router, private subscriptions: RxHelper) {
   }
 
+  ngOnInit() {
+    let subscription = this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.uid = auth.uid;
+        console.log("New agency admin uid: " + this.uid);
+        this.af.database.object(Constants.APP_STATUS+"/userPublic/" + this.uid).subscribe(user => {
+          this.agencyAdminName = user.firstName;
+        });
+      } else {
+        this.router.navigateByUrl(Constants.LOGIN_PATH);
+      }
+    });
+    this.subscriptions.add(subscription);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.releaseAll();
+  }
+
+  private showAlert(error: boolean) {
+    if (error) {
+      this.errorInactive = false;
+      let subscription = Observable.timer(Constants.ALERT_DURATION).subscribe(() => {
+        this.errorInactive = true;
+      });
+      this.subscriptions.add(subscription);
+    } else {
+      this.successInactive = false;
+      let subscription = Observable.timer(Constants.ALERT_DURATION).subscribe(() => {
+        this.successInactive = true;
+      });
+      this.subscriptions.add(subscription);
+    }
+  }
+
+  /**
+   * Returns false and specific error messages-
+   * @returns {boolean}
+   */
+  private validate() {
+
+    this.alerts = {};
+    if (!(this.agencyAddressLine1)) {
+      this.alerts[this.agencyAddressLine1] = true;
+      this.errorMessage = "AGENCY_ADMIN.UPDATE_DETAILS.NO_ADDRESS_1";
+      return false;
+    } else if (!(this.agencyCity)) {
+      this.alerts[this.agencyCity] = true;
+      this.errorMessage = "AGENCY_ADMIN.UPDATE_DETAILS.NO_CITY";
+      return false;
+    } else if (!(this.agencyPhone)) {
+      this.alerts[this.agencyPhone] = true;
+      this.errorMessage = "AGENCY_ADMIN.UPDATE_DETAILS.NO_PHONE";
+      return false;
+    }
+    return true;
+  }
 }
