0607178c96309d0319abbaae3a246684c6a74fc9
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-04-21 11:12:44 +0100

    WIP response plans

--- src/app/response-plans/create-edit-response-plan/create-edit-response-plan.component.ts
+++ src/app/response-plans/create-edit-response-plan/create-edit-response-plan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-edit-response-plan',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEditResponsePlanComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.router.navigateByUrl('response-plans');
   }
 
 }
--- src/app/response-plans/response-plans.component.ts
+++ src/app/response-plans/response-plans.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-response-plans',
   templateUrl: './response-plans.component.html',
   styleUrls: ['./response-plans.component.css']
 })
+
 export class ResponsePlansComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToNewOrEditPlan() {
+    this.router.navigateByUrl('response-plans/create-edit-response-plan');
   }
 
 }
