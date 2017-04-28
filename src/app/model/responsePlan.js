058ce4165420844cecb098895032df4989513df5
Sanjaya Don Ranasinge <sanjaya@rolleragency.co.uk>
2017-04-28 17:16:57 +0100

    Conflicts

---
+++ src/app/model/responsePlan.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * Created by Sanjaya on 27/04/2017.
+ */
+var ResponsePlan = (function () {
+    function ResponsePlan() {
+    }
+    return ResponsePlan;
+}());
+exports.ResponsePlan = ResponsePlan;
