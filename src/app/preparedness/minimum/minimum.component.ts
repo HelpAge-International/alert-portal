913f9fa876a2d6fc87f20fa7bb3da9ecfddfe692
kplese <kresimir.plese@gmail.com>
2017-05-04 21:53:39 +0200

    Preparedness filters - WIP

--- src/app/preparedness/minimum/minimum.component.ts
+++ src/app/preparedness/minimum/minimum.component.ts
@@ -19,10 +19,26 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 	ACTION_LEVEL = Constants.ACTION_LEVEL;
 	ACTION_TYPE = Constants.ACTION_TYPE;
 	private subscriptions: RxHelper;
-	private uid: string = "qbyONHp4xqZy2eUw0kQHU7BAcov1";
+	private uid: string = "C1T4Mx2gZFTFSq8CTxSEsyJDr2j2"; // Country Admin TODO remove
 	private actions: any[] = [];
 	private users: any[] = [];
+	private assignedToUsers: any[] = [];
+	private departments: any[] = [];
+	private countryId = null;
 	private actionStatus = ActionStatus;
+	private ActionStatusEnum = Object.keys(ActionStatus).map(k => ActionStatus[k]).filter(v => typeof v === "string") as string[];
+	private ActionTypeEnum = Object.keys(ActionType).map(k => ActionType[k]).filter(v => typeof v === "string") as string[];
+
+	private statusSelected = "-1";
+	private departmentSelected = "-1";
+	private typeSelected = "-1";
+	private userSelected = "-1";
+	private agencyNetworkSelected = "-1";
+	private assignedToUser = "me";
+	private assignedToUserKey;
+	private assignedToAnyone = false;
+
+	private allArchived = false;
 
 	constructor(private af: AngularFire, private router: Router) {
 		this.subscriptions = new RxHelper;
@@ -31,7 +47,8 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 	ngOnInit() {
 		let subscription = this.af.auth.subscribe(auth => {
 			if (auth) {
-				this.uid = auth.uid;
+				//this.uid = auth.uid; TODO remove comment
+				this.assignedToUserKey = this.uid;
 				this.subscriptions.add(this.af.database.list(Constants.APP_STATUS+'/action/').subscribe(_ => {
 					this.actions = [];
 					_.map(actions => {
@@ -53,7 +70,6 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 
 							this.subscriptions.add(this.af.database.object(Constants.APP_STATUS+'/userPublic/' + userKey).subscribe(_ => {
 								if (_.$exists()){
-									console.log(_);
 									this.users[userKey] = _.firstName + " " + _.lastName;
 									actions[action].assigned = true;
 								}
@@ -73,7 +89,6 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 								actions[action].notes = _;
 								actions[action].notes.map(note => {
 									let uploadByUser = note.uploadBy;
-									console.log(note);
 									this.subscriptions.add(this.af.database.object(Constants.APP_STATUS+'/userPublic/' + uploadByUser).subscribe(_ => {
 										if (_.$exists()){
 											note.uploadByUser = _.firstName + " " + _.lastName;
@@ -81,12 +96,14 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 										else{
 											note.uploadByUser = "N/A";
 										}
-
-										return note;
 									}));
+
+									return note;
 								});
 								
 							}));
+
+							
 
 							if (actions[action].level == ActionLevel.MPA){
 								this.actions.push(actions[action]);
@@ -94,6 +111,41 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 						});
 					});
 				}));
+
+				this.subscriptions.add(this.af.database.object(Constants.APP_STATUS+'/agency/' + this.uid + '/departments').subscribe(_ => {
+					if (_.$exists()){
+						//console.log(_);
+						this.departments = Object.keys(_);
+					}
+					else{
+						this.departments = [];	
+					}
+
+				}));
+
+				this.subscriptions.add(this.af.database.object(Constants.APP_STATUS+'/administratorCountry/' + this.uid + '/countryId').subscribe(country => {
+					if (country.$exists()) {
+						this.countryId = country.$value
+console.log(this.countryId);
+						this.assignedToUsers = [];
+						this.subscriptions.add(this.af.database.list(Constants.APP_STATUS+'/staff/' + this.countryId).subscribe(staff => {
+							this.assignedToUsers = staff.map(member => {
+								let userId = member.$key;
+								this.subscriptions.add(this.af.database.object(Constants.APP_STATUS+'/userPublic/' + userId).subscribe(_ => {
+									if (_.$exists()){
+										member.fullName = _.firstName + " " + _.lastName;
+									}
+									else{
+										member.fullName = "";
+									}
+								}));
+
+								return member;
+							});
+						
+						}));
+					}
+				}));				
 
 			    this.subscriptions.add(subscription);
 			} else {
@@ -136,6 +188,38 @@ export class MinimumPreparednessComponent implements OnInit, OnDestroy {
 
 	private deleteNote(note, action){
 		this.af.database.list(Constants.APP_STATUS+'/note/' + action.key + '/' + note.$key).remove();
+	}
+
+	private filter() {
+		if (this.userSelected == "-1"){
+			this.assignedToUser = "me";
+			this.assignedToUserKey = this.uid;
+			this.assignedToAnyone = false;
+		} else if (this.userSelected == "0"){
+			this.assignedToUser = "Anyone";
+			this.assignedToAnyone = true;
+		} else {
+			let users = this.assignedToUsers.filter(user => {
+				return user.$key == this.userSelected
+			});
+
+			if (users.length > 0){
+				this.assignedToUser = users[0].fullName;
+				this.assignedToUserKey = users[0].$key;
+				this.assignedToAnyone = false;
+			}
+
+		}
+		// console.log(this.statusSelected);
+		// console.log(this.departmentSelected);
+		// console.log(this.typeSelected);
+		// console.log(this.userSelected);
+		// console.log(this.agencyNetworkSelected);
+	}
+
+	private showAllArchived(show){
+		this.allArchived = show;
+		console.log(this.allArchived);
 	}
 
 }
