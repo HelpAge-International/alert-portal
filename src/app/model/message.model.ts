1af6d36108c6b6890deba51ad691678d278632a1
Lucian Moldovan <moldovan.lucian.marius@gmail.com>
2017-05-25 12:01:46 +0300

    Adds the message model.

---
+++ src/app/model/message.model.ts
@@ -0,0 +1,28 @@
+import { BaseModel } from "./base.model";
+import { AlertMessageModel } from "./alert-message.model";
+
+export class MessageModel extends BaseModel {
+  public senderId: string;
+  public title: string;
+  public content: string;
+  public time: number;
+  public userType: any[];
+
+  constructor() { 
+    super(); 
+    this.userType = [];
+}
+
+  validate(excludedFields = []): AlertMessageModel {
+    if (!this.title && !this.isExcluded('title', excludedFields)) {
+      return new AlertMessageModel('MESSAGES.NO_TITLE_ERROR');
+    }
+    if (!this.content && !this.isExcluded('content', excludedFields)) {
+      return new AlertMessageModel('MESSAGES.NO_CONTENT_ERROR');
+    }
+    if(this.userType.length < 1 && !this.isExcluded('content', excludedFields)){
+      return new AlertMessageModel('MESSAGES.NO_RECIPIENTS_ERROR');
+    }
+    return null;
+  }
+}
