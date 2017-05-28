518b6c08f2393bb007fd0787e1e0a80e8eac78d3
Jordan Fisher <thementalgoose@gmail.com>
2017-05-28 15:11:44 +0100

    Added Calendar saving functionality

--- src/app/dashboard/dashboard-seasonal-calendar/dashboard-seasonal-calendar.component.ts
+++ src/app/dashboard/dashboard-seasonal-calendar/dashboard-seasonal-calendar.component.ts
@@ -4,7 +4,9 @@ import {Router} from "@angular/router";
 import {Subject} from "rxjs";
 import {Constants} from "../../utils/Constants";
 import {ModelSeason} from "../../model/season.model";
+import {ColourSelector} from "../../utils/ColourSelector";
 declare var Chronoline, document, DAY_IN_MILLISECONDS, isFifthDay, prevMonth, nextMonth: any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-dashboard-seasonal-calendar',
@@ -14,6 +16,10 @@ declare var Chronoline, document, DAY_IN_MILLISECONDS, isFifthDay, prevMonth, ne
 
 export class DashboardSeasonalCalendarComponent implements OnInit, OnDestroy {
 
+  private static WEEK_SPAN_DAY_MULTIPLIER = 30;
+  private static MONTH_SPAN_DAY_MULTIPLIER = 91;
+  private static RANGE_SPAN_DAYS = 365;
+
   // TODO - Check when other users are implemented
   private USER_TYPE: string = 'administratorCountry';
 
@@ -21,38 +27,22 @@ export class DashboardSeasonalCalendarComponent implements OnInit, OnDestroy {
   private countryId: string;
   private currentSpanMultiplierIsMonth: boolean = true;
   private init: boolean = false;
-
   private currentChronolineInstance;
 
-  private static WEEK_SPAN_DAY_MULTIPLIER = 30;
-  private static MONTH_SPAN_DAY_MULTIPLIER = 91;
-  private static RANGE_SPAN_DAYS = 365;
-
-  public setMonthView() {
-    this.currentSpanMultiplierIsMonth = true;
-    this.reinitCalendar();
-  }
-
-  public setWeekView() {
-    this.currentSpanMultiplierIsMonth = false;
-    this.reinitCalendar();
-  }
-
-  private reinitCalendar() {
-    // Stop synchronisation issue (ie. Someone click Weekly before calendar is loaded)
-    if (this.init) {
-      this.initCalendar();
-    }
-  }
-
+  private addSeasonName: string;
+  private addSeasonStart: string;
+  private addSeasonEnd: string;
+  private seasons: string;
+  private addSeasonColour: string;
+  private colours: ColourSelector[] = ColourSelector.list();
   private seasonEvents = [];
-
   private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(private af: AngularFire, private router: Router) {
   }
 
   ngOnInit() {
+    console.log(this.colours);
     this.af.auth.takeUntil(this.ngUnsubscribe).subscribe(user => {
       if (user) {
         this.uid = user.auth.uid;
@@ -70,6 +60,38 @@ export class DashboardSeasonalCalendarComponent implements OnInit, OnDestroy {
     this.ngUnsubscribe.complete();
   }
 
+
+  /**
+   * Ran when "View as months" is clicked. Changes filter and re-initialised the Chronoline map
+   */
+  public setMonthView() {
+    this.currentSpanMultiplierIsMonth = true;
+    this.reinitCalendar();
+  }
+
+  /**
+   * Ran when "View as weeks" is clicked. Changes filter and re-initialised the Chronoline map
+   */
+  public setWeekView() {
+    this.currentSpanMultiplierIsMonth = false;
+    this.reinitCalendar();
+  }
+
+  /**
+   * Reinitialises the calendar
+   */
+  private reinitCalendar() {
+    // Stop synchronisation issue (ie. Someone click Weekly before calendar is loaded)
+    if (this.init) {
+      this.initCalendar();
+    }
+  }
+
+  /**
+   * Pulls all the data from /seasons/ node in firebase and initialises the calendar
+   * Note: Object is used here because the map needs to be re-initialised. Object allows
+   * me to not have to track which item has changed opposed to list
+   */
   public getAllSeasonsForCountryId(countryId: string) {
     this.af.database.object(Constants.APP_STATUS + "/season/" + countryId, {preserveSnapshot: true})
       .takeUntil(this.ngUnsubscribe)
@@ -88,25 +110,34 @@ export class DashboardSeasonalCalendarComponent implements OnInit, OnDestroy {
       });
   }
 
+  /**
+   * Get the country Id
+   */
   private getCountryId() {
     let promise = new Promise((res, rej) => {
-      this.af.database.object(Constants.APP_STATUS + "/" + this.USER_TYPE + "/" + this.uid + "/countryId")
+      this.af.database.object(Constants.APP_STATUS + "/" + this.USER_TYPE + "/" + this.uid + "/countryId", {preserveSnapshot: true})
         .takeUntil(this.ngUnsubscribe)
-        .subscribe((countryId: any) => {
-          this.countryId = countryId.$value;
+        .subscribe((countryId) => {
+          this.countryId = countryId.val();
           res(true);
         });
     });
     return promise;
   }
 
+  /**
+   * Model Chronoline item. Chronoline only shows span of max and min items, so blank item is shown which spans +/- RANGE_SPAN_DAYS
+   */
   public static spanModelCalendar() {
     // Blank calendar object which will allow the full span to load properly of the calendar
     let daysPlusMinus = DashboardSeasonalCalendarComponent.RANGE_SPAN_DAYS;
     let d = new Date();
-    return new ModelSeason("#00131D50", d.getTime() - (daysPlusMinus * (1000 * 60 * 60 * 24)),  d.getTime() + (daysPlusMinus * (1000 * 60 * 60 * 24)), "Name")
+    return new ModelSeason("#00131D50", "Name", d.getTime() - (daysPlusMinus * (1000 * 60 * 60 * 24)),  d.getTime() + (daysPlusMinus * (1000 * 60 * 60 * 24)))
   }
 
+  /**
+   * Initialise the calendar
+   */
   private initCalendar() {
     // To show weekly calendar ----> Change visibleSpan to 'DAY_IN_MILLISECONDS * 30'
     document.getElementById("target2").innerHTML = "";
@@ -133,8 +164,49 @@ export class DashboardSeasonalCalendarComponent implements OnInit, OnDestroy {
     this.router.navigateByUrl(Constants.LOGIN_PATH);
   }
 
+  public saveCalendarInfo() {
+    console.log("Saving!");
+    console.log(this.addSeasonColour);
+    if (this.addSeasonName == null) {
+      //TODO: Error handling!
+      console.error("Season Name Error Handling!");
+      return;
+    }
+    if (this.addSeasonColour == null) {
+      //TODO: Error handling!
+      console.error("Season Colour Error Handling!");
+      return;
+    }
+    if (this.addSeasonStart == null) {
+      //TODO: Error handling
+      console.error("Start Season Error Handling!");
+      return;
+    }
+    if (this.addSeasonEnd == null) {
+      //TODO: Error handling
+      console.error("End Season Error Handling!");
+      return;
+    }
+    let season: ModelSeason = new ModelSeason(this.addSeasonColour, this.addSeasonName,
+      DashboardSeasonalCalendarComponent.convertYYYYMMDDToUTC(this.addSeasonStart),
+      DashboardSeasonalCalendarComponent.convertYYYYMMDDToUTC(this.addSeasonEnd));
+    this.af.database.list(Constants.APP_STATUS + "/season/" + this.countryId + "/").push(season);
+    // Below line wasn't working when I was trying to hide it!
+    // jQuery("#add_calendar").modal("hide");
+  }
+
+  public setCurrentColour(colourCode: string) {
+    this.addSeasonColour = colourCode;
+  }
+
+  public static convertYYYYMMDDToUTC(date: string) {
+    return new Date(date).getTime();
+  }
 }
 
+/**
+ * Model chonoline Event item - The objects need to match the info below
+ */
 export class ChronolineEvent {
   private dates: Date[];
   private title: string;
