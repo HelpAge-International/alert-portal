2565abd86d2bc8bbd71c4bc60f49b78e7cbdc981
Fei Xie <fei@rolleragency.co.uk>
2017-05-30 13:31:02 +0100

    meeting request complete

--- src/app/dashboard/facetoface-meeting-request/facetoface-meeting-request.component.ts
+++ src/app/dashboard/facetoface-meeting-request/facetoface-meeting-request.component.ts
@@ -5,6 +5,8 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 import {Constants} from "../../utils/Constants";
 import {Countries} from "../../utils/Enums";
 import {AgencyService} from "../../services/agency-service.service";
+import {ModelFaceToFce} from "./facetoface.model";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-facetoface-meeting-request',
@@ -20,8 +22,12 @@ export class FacetofaceMeetingRequestComponent implements OnInit, OnDestroy {
   private countryId: string;
   private agencyId: string;
 
+  private displayList: ModelFaceToFce[] = [];
+  private agencySelectionMap = new Map();
+  private emails: string;
 
-  constructor(private af: AngularFire, private router: Router, private route: ActivatedRoute, private agencyService: AgencyService) {
+
+  constructor(private af: AngularFire, private router: Router, private route: ActivatedRoute, private agencyService: AgencyService, private userService: UserService) {
   }
 
   ngOnInit() {
@@ -46,13 +52,73 @@ export class FacetofaceMeetingRequestComponent implements OnInit, OnDestroy {
   }
 
   private initData(countryId, agencyId) {
-    this.agencyService.getAllAgencySameCountry(countryId, agencyId);
+    // let allAgencySameCountry = this.agencyService.getAllAgencySameCountry(countryId, agencyId);
+    // console.log(allAgencySameCountry[0]);
+    this.getAllAgencySameCountry(countryId, agencyId);
+  }
+
+  checkAgency(item, value) {
+    this.agencySelectionMap.set(item.countryDirectorEmail, value);
+  }
+
+  sendEmail() {
+    console.log(this.agencySelectionMap);
+    this.agencySelectionMap.forEach((v, k) => {
+      if (v) {
+        if (this.emails) {
+          this.emails.concat(";" + k);
+        } else {
+          this.emails = k;
+        }
+      }
+    });
   }
 
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
     this.agencyService.unSubscribeNow();
+  }
+
+  getAllAgencySameCountry(countryId, agencyId) {
+    this.agencyService.getCountryOffice(countryId, agencyId)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(country => {
+        this.agencyService.getAllCountryOffices()
+          .takeUntil(this.ngUnsubscribe)
+          .subscribe(agencies => {
+            agencies = agencies.filter(agency => agency.$key != agencyId);
+            agencies.forEach(agency => {
+              let countries = Object.keys(agency).map(key => {
+                let temp = agency[key];
+                temp["countryId"] = key;
+                return temp;
+              });
+              countries = countries.filter(countryItem => countryItem.location == country.location);
+              if (countries.length > 0) {
+                let faceToface = new ModelFaceToFce();
+                faceToface.agencyId = agency.$key;
+                faceToface.countryId = countries[0].countryId;
+                this.displayList.push(faceToface);
+
+                this.agencyService.getAgency(faceToface.agencyId)
+                  .takeUntil(this.ngUnsubscribe)
+                  .subscribe(agency => {
+                    faceToface.agencyName = agency.name;
+                    faceToface.agencyLogoPath = agency.logoPath;
+                  });
+
+                this.agencyService.getCountryDirector(countryId)
+                  .takeUntil(this.ngUnsubscribe)
+                  .subscribe(director => {
+                    faceToface.countryDirectorId = director.$key;
+                    faceToface.countryDirectorName = director.firstName + " " + director.lastName;
+                    faceToface.countryDirectorEmail = director.email;
+                  });
+              }
+            });
+          });
+      });
   }
 
 }
--- src/app/services/agency-service.service.ts
+++ src/app/services/agency-service.service.ts
@@ -3,6 +3,7 @@ import {Constants} from "../utils/Constants";
 import {AngularFire} from "angularfire2";
 import {Subject} from "rxjs/Subject";
 import {ModelFaceToFce} from "../dashboard/facetoface-meeting-request/facetoface.model";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class AgencyService {
@@ -13,34 +14,65 @@ export class AgencyService {
   }
 
   getAgencyId(agencyAdminId) {
-    let subscription = this.af.database.object(Constants.APP_STATUS + "/administratorAgency/" + agencyAdminId + "/agencyId")
+    return this.af.database.object(Constants.APP_STATUS + "/administratorAgency/" + agencyAdminId + "/agencyId")
       .map(id => {
         if (id.$value) {
           return id.$value;
         }
       });
-    return subscription;
   }
 
-  getAllAgencySameCountry(countryId, agencyId){
-    this.af.database.object(Constants.APP_STATUS+"/countryOffice/"+agencyId+"/"+countryId)
-      .map(country => {
-        let displayList:ModelFaceToFce[] = [];
-        this.af.database.list(Constants.APP_STATUS+"/countryOffice/")
+  getAgency(agencyId) {
+    return this.af.database.object(Constants.APP_STATUS + "/agency/" + agencyId);
+  }
+
+  getCountryOffice(countryId, agencyId) {
+    return this.af.database.object(Constants.APP_STATUS + "/countryOffice/" + agencyId + "/" + countryId);
+  }
+
+  getAllCountryOffices() {
+    return this.af.database.list(Constants.APP_STATUS + "/countryOffice/");
+  }
+
+  getCountryDirector(countryId) {
+    return this.af.database.object(Constants.APP_STATUS + "/directorCountry/" + countryId)
+      .flatMap(directorId => {
+        return this.af.database.object(Constants.APP_STATUS + "/userPublic/" + directorId.$value);
+      });
+  }
+
+  getAllAgencySameCountry(countryId, agencyId) {
+
+    let displayList: ModelFaceToFce[] = [];
+    this.af.database.object(Constants.APP_STATUS + "/countryOffice/" + agencyId + "/" + countryId)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(country => {
+
+
+        this.af.database.list(Constants.APP_STATUS + "/countryOffice/")
           .takeUntil(this.ngUnsubscribe)
           .subscribe(agencies => {
+            agencies = agencies.filter(agency => agency.$key != agencyId);
             // console.log(agencies);
-            agencies.forEach(agency =>{
-              let countries = Object.keys(agency).map(key => agency[key]);
-              console.log(countries);
-              countries.forEach(country =>{
-
+            agencies.forEach(agency => {
+              // console.log(agency);
+              let countries = Object.keys(agency).map(key => {
+                let temp = agency[key];
+                temp["countryId"] = key;
+                return temp;
               });
+              countries = countries.filter(countryItem => countryItem.location == country.location);
+              // console.log(countries);
+              if (countries.length > 0) {
+                let faceToface = new ModelFaceToFce();
+                faceToface.agencyId = agency.$key;
+                faceToface.countryId = countries[0].countryId;
+                displayList.push(faceToface);
+              }
             });
           });
-        return country;
-      })
-      .subscribe(x=>{console.log(x)})
+      });
+    return displayList;
   }
 
   unSubscribeNow() {
