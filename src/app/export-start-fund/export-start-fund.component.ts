6a030edff2bada387d28938f09066a93adbede32
Gishan Don Ranasinghe <dongishan@users-MacBook-Air.local>
2017-06-12 17:04:04 +0100

    Preparations for the frontend forms

--- src/app/export-start-fund/export-start-fund.component.ts
+++ src/app/export-start-fund/export-start-fund.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-export-start-fund',
-  templateUrl: './export-start-fund.component.html',
+  templateUrl: './export-start-fund-test.component.html',
   styleUrls: ['./export-start-fund.component.css']
 })
 export class ExportStartFundComponent implements OnInit {
