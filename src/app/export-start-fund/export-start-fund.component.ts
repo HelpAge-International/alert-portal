c0b2adfe3c2a48ab85016ded997191a0e3f86125
Gishan Don Ranasinghe <dongishan@users-MacBook-Air.local>
2017-06-13 17:19:51 +0100

    Data loading into project narrative WIP

--- src/app/export-start-fund/export-start-fund.component.ts
+++ src/app/export-start-fund/export-start-fund.component.ts
@@ -6,9 +6,8 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 import {ResponsePlan} from "../model/responsePlan";
 import {UserService} from "../services/user.service";
 import {
-  BudgetCategory, UserType
+  BudgetCategory, ResponsePlanSectors, UserType
 } from "../utils/Enums";
-import {ModelPlanActivity} from "../model/plan-activity.model";
 
 @Component({
   selector: 'app-export-start-fund',
@@ -22,41 +21,27 @@ export class ExportStartFundComponent implements OnInit, OnDestroy {
   private uid: string;
   private countryId: string;
   @Input() responsePlanId: string;
-  private responsePlanToShow: ResponsePlan = new ResponsePlan;
   private ngUnsubscribe: Subject<void> = new Subject<void>();
+
+  private responsePlanSectors = ResponsePlanSectors;
+  private responsePlan: ResponsePlan = new ResponsePlan;
   private planLeadName: string = '';
-  private sectors: any[];
-  private partnerList: string[] = [];
-  private intendToVisuallyDoc: string = '';
-  private mediaType: any;
-  private totalInputs: number;
-  private totalOfAllCosts: number;
-  private total: number;
-  private transportBudget: number;
-  private securityBudget: number;
-  private logisticsAndOverheadsBudget: number;
-  private staffingAndSupportBudget: number;
-  private monitoringAndEvolutionBudget: number;
-  private capitalItemsBudget: number;
-  private managementSupportPercentage: any;
-  private transportNarrative: string;
-  private securityNarrative: string;
-  private logisticsAndOverheadsNarrative: string;
-  private staffingAndSupportNarrative: string;
-  private monitoringAndEvolutionNarrative: string;
-  private capitalItemsNarrative: string;
-  private managementSupportNarrative: string;
-  private activityInfoMap = new Map();
-  private activityMap = new Map();
+  private planLeadEmail: string = '';
+  private planLeadPhone: string = '';
+  private sectorsRelatedToMap = new Map<number,boolean>();
 
   constructor(private af: AngularFire, private router: Router, private userService: UserService, private route: ActivatedRoute) {
   }
+
+  /**
+   * Lifecycle Functions
+   */
 
   ngOnInit() {
     this.af.auth.takeUntil(this.ngUnsubscribe).subscribe(user => {
       if (user) {
         this.uid = user.auth.uid;
-        this.loadData();
+        this.downloadData();
       } else {
         this.navigateToLogin();
       }
@@ -72,7 +57,7 @@ export class ExportStartFundComponent implements OnInit, OnDestroy {
    * Private Functions
    */
 
-  private loadData() {
+  private downloadData() {
     this.userService.getUserType(this.uid)
       .takeUntil(this.ngUnsubscribe)
       .subscribe(usertype => {
@@ -82,31 +67,58 @@ export class ExportStartFundComponent implements OnInit, OnDestroy {
             .subscribe((params: Params) => {
               if (params["countryId"]) {
                 this.countryId = params["countryId"];
-                this.handleLoadResponsePlan();
+                this.downloadResponsePlanData();
               }
             })
         } else {
           this.getCountryId().then(() => {
-            this.handleLoadResponsePlan();
+            this.downloadResponsePlanData();
           });
         }
       });
   }
 
-  private handleLoadResponsePlan() {
-    if (this.responsePlanId) {
-      this.loadResponsePlanData();
-    } else {
+  private downloadResponsePlanData() {
+    if (!this.responsePlanId) {
       this.route.params
         .takeUntil(this.ngUnsubscribe)
         .subscribe((params: Params) => {
           if (params["id"]) {
             this.responsePlanId = params["id"];
-            this.loadResponsePlanData();
           }
         });
     }
+    let responsePlansPath: string = Constants.APP_STATUS + '/responsePlan/' + this.countryId + '/' + this.responsePlanId;
+    this.af.database.object(responsePlansPath)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((responsePlan: ResponsePlan) => {
+        this.responsePlan = responsePlan;
+        console.log(responsePlan);
+
+        responsePlan.sectorsRelatedTo.forEach(sector =>{
+          this.sectorsRelatedToMap.set(sector,true);
+        });
+
+        this.bindProjectLeadData(responsePlan);
+      });
   }
+
+  private bindProjectLeadData(responsePlan: ResponsePlan) {
+    if (responsePlan.planLead) {
+      this.userService.getUser(responsePlan.planLead)
+        .takeUntil(this.ngUnsubscribe)
+        .subscribe(user => {
+          console.log(user);
+          this.planLeadName = user.title + " " + user.firstName + " " + user.lastName;
+          this.planLeadEmail = user.email;
+          this.planLeadPhone = user.phone;
+        });
+    }
+  }
+
+  /**
+   * Utility Functions
+   */
 
   private getCountryId() {
     let promise = new Promise((res, rej) => {
@@ -120,162 +132,8 @@ export class ExportStartFundComponent implements OnInit, OnDestroy {
     return promise;
   }
 
-  private loadResponsePlanData() {
-    console.log("response plan id: " + this.responsePlanId);
-    let responsePlansPath: string = Constants.APP_STATUS + '/responsePlan/' + this.countryId + '/' + this.responsePlanId;
-
-    this.af.database.object(responsePlansPath)
-      .takeUntil(this.ngUnsubscribe)
-      .subscribe((responsePlan: ResponsePlan) => {
-        this.responsePlanToShow = responsePlan;
-
-        this.loadSection1PlanLead(responsePlan);
-        this.loadSection3(responsePlan);
-        this.loadSection7(responsePlan);
-        this.loadSection8(responsePlan);
-        this.loadSection10(responsePlan);
-      });
-  }
-
-  private loadSection1PlanLead(responsePlan: ResponsePlan) {
-    if (responsePlan.planLead) {
-      this.userService.getUser(responsePlan.planLead)
-        .takeUntil(this.ngUnsubscribe)
-        .subscribe(user => {
-          this.planLeadName = user.firstName + " " + user.lastName;
-          console.log("Lead Name: " + this.planLeadName);
-        });
-    } else {
-      this.planLeadName = 'Unassigned';
-    }
-  }
-
-  private loadSection3(responsePlan: ResponsePlan) {
-    console.log(responsePlan);
-    if (responsePlan.sectors) {
-      this.sectors = Object.keys(responsePlan.sectors).map(key => {
-        let sector = responsePlan.sectors[key];
-        sector["id"] = Number(key);
-        return sector;
-      });
-    }
-    if (responsePlan.partnerOrganisations) {
-      this.partnerList = [];
-      let partnerIds = Object.keys(responsePlan.partnerOrganisations).map(key => responsePlan.partnerOrganisations[key]);
-      partnerIds.forEach(id => {
-        this.userService.getOrganisationName(id)
-          .takeUntil(this.ngUnsubscribe)
-          .subscribe(organisation => {
-            this.partnerList.push(organisation.organisationName);
-          })
-      });
-    }
-  }
-
-  private loadSection7(responsePlan: ResponsePlan) {
-    if (this.sectors) {
-      Object.keys(responsePlan.sectors).forEach(sectorKey => {
-
-        let infoData = {};
-        infoData["sourcePlan"] = responsePlan.sectors[sectorKey]["sourcePlan"];
-        infoData["bullet1"] = responsePlan.sectors[sectorKey]["bullet1"];
-        infoData["bullet2"] = responsePlan.sectors[sectorKey]["bullet2"];
-        this.activityInfoMap.set(Number(sectorKey), infoData);
-
-        let activitiesData: {} = responsePlan.sectors[sectorKey]["activities"];
-        let moreData: {}[] = [];
-        Object.keys(activitiesData).forEach(key => {
-          let beneficiary = [];
-          activitiesData[key]["beneficiary"].forEach(item => {
-            beneficiary.push(item);
-          });
-          let model = new ModelPlanActivity(activitiesData[key]["name"], activitiesData[key]["output"], activitiesData[key]["indicator"], beneficiary);
-          moreData.push(model);
-          this.activityMap.set(Number(sectorKey), moreData);
-        });
-      });
-    }
-  }
-
-  private loadSection8(responsePlan: ResponsePlan) {
-    if (responsePlan.monAccLearning) {
-      if (responsePlan.monAccLearning['isMedia']) {
-        if (responsePlan.monAccLearning['mediaFormat'] || responsePlan.monAccLearning['mediaFormat'] == 0) {
-          this.intendToVisuallyDoc = "GLOBAL.YES";
-          this.mediaType = Constants.MEDIA_TYPES[responsePlan.monAccLearning['mediaFormat']];
-        } else {
-          this.intendToVisuallyDoc = "GLOBAL.YES";
-          this.mediaType = '';
-        }
-      } else {
-        this.intendToVisuallyDoc = "GLOBAL.NO";
-        this.mediaType = '';
-      }
-    } else {
-      this.intendToVisuallyDoc = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    }
-  }
-
-  private loadSection10(responsePlan: ResponsePlan) {
-    if (responsePlan.budget) {
-      this.totalInputs = responsePlan.budget['totalInputs'] ? responsePlan.budget['totalInputs'] : 0;
-      this.totalOfAllCosts = responsePlan.budget['totalOfAllCosts'] ? responsePlan.budget['totalOfAllCosts'] : 0;
-      this.total = responsePlan.budget['total'] ? responsePlan.budget['total'] : 0;
-
-      if (responsePlan.budget['item']) {
-        this.transportBudget = responsePlan.budget['item'][BudgetCategory.Transport] ? responsePlan.budget['item'][BudgetCategory.Transport]['budget'] : 0;
-        this.transportNarrative = responsePlan.budget['item'][BudgetCategory.Transport] ? responsePlan.budget['item'][BudgetCategory.Transport]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.securityBudget = responsePlan.budget['item'][BudgetCategory.Security] ? responsePlan.budget['item'][BudgetCategory.Security]['budget'] : 0;
-        this.securityNarrative = responsePlan.budget['item'][BudgetCategory.Security] ? responsePlan.budget['item'][BudgetCategory.Security]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.logisticsAndOverheadsBudget = responsePlan.budget['item'][BudgetCategory.Logistics] ? responsePlan.budget['item'][BudgetCategory.Logistics]['budget'] : 0;
-        this.logisticsAndOverheadsNarrative = responsePlan.budget['item'][BudgetCategory.Logistics] ? responsePlan.budget['item'][BudgetCategory.Logistics]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.staffingAndSupportBudget = responsePlan.budget['item'][BudgetCategory.Staffing] ? responsePlan.budget['item'][BudgetCategory.Staffing]['budget'] : 0;
-        this.staffingAndSupportNarrative = responsePlan.budget['item'][BudgetCategory.Staffing] ? responsePlan.budget['item'][BudgetCategory.Staffing]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.monitoringAndEvolutionBudget = responsePlan.budget['item'][BudgetCategory.Monitoring] ? responsePlan.budget['item'][BudgetCategory.Monitoring]['budget'] : 0;
-        this.monitoringAndEvolutionNarrative = responsePlan.budget['item'][BudgetCategory.Monitoring] ? responsePlan.budget['item'][BudgetCategory.Monitoring]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.capitalItemsBudget = responsePlan.budget['item'][BudgetCategory.CapitalItems] ? responsePlan.budget['item'][BudgetCategory.CapitalItems]['budget'] : 0;
-        this.capitalItemsNarrative = responsePlan.budget['item'][BudgetCategory.CapitalItems] ? responsePlan.budget['item'][BudgetCategory.CapitalItems]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-        this.managementSupportPercentage = responsePlan.budget['item'][BudgetCategory.ManagementSupport] ? responsePlan.budget['item'][BudgetCategory.ManagementSupport]['budget'] + '%' : '0%';
-        this.managementSupportNarrative = responsePlan.budget['item'][BudgetCategory.ManagementSupport] ? responsePlan.budget['item'][BudgetCategory.ManagementSupport]['narrative'] : "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-
-      } else {
-        this.assignDefaultValues();
-      }
-    } else {
-      this.assignDefaultValues();
-    }
-  }
-
-  private assignDefaultValues() {
-
-    this.transportBudget = 0;
-    this.securityBudget = 0;
-    this.logisticsAndOverheadsBudget = 0;
-    this.staffingAndSupportBudget = 0;
-    this.monitoringAndEvolutionBudget = 0;
-    this.capitalItemsBudget = 0;
-    this.managementSupportPercentage = 0;
-
-    this.totalInputs = 0;
-    this.totalOfAllCosts = 0;
-    this.total = 0;
-
-    this.transportNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.securityNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.logisticsAndOverheadsNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.staffingAndSupportNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.monitoringAndEvolutionNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.capitalItemsNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-    this.managementSupportNarrative = "RESPONSE_PLANS.NO_INFO_TO_SHOW";
-  }
-
   private navigateToLogin() {
     this.router.navigateByUrl(Constants.LOGIN_PATH);
   }
+
 }
