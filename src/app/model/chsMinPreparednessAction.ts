990e32bc95cc8dbf615c33ecd42fa21eafaad455
kplese <kresimir.plese@gmail.com>
2017-06-15 01:17:11 +0200

    CHS now shown in minimum preparedness page - ALT-1373

--- src/app/model/chsMinPreparednessAction.ts
+++ src/app/model/chsMinPreparednessAction.ts
@@ -1,12 +1,13 @@
 /**
  * Created by Sanjaya on 09/03/2017.
  */
-import {ActionType} from '../utils/Enums';
+import {ActionType, ActionLevel} from '../utils/Enums';
 
 export class ChsMinPreparednessAction {
   public task: string;
   public type: ActionType;
   public createdAt: number;
+  public level: ActionLevel;
 
   // constructor(task: string, type: ActionType, createdAt: number) {
   //   this.task = task;
