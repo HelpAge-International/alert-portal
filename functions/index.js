44c5a5b5f027a90aa98c6a7c3cad60302f23fcda
Fei Xie <fei@rolleragency.co.uk>
2017-06-15 16:16:43 +0100

    Wip

--- functions/index.js
+++ functions/index.js
@@ -128,6 +128,7 @@ exports.handleResponsePlans = functions.database.ref('/sand/responsePlan/{countr
     console.log(event.params["countryId"]);
 
     let countryId = event.params["countryId"];
+    let responsePlanId = event.data.key;
 
     admin.database().ref('/sand/administratorCountry').orderByChild("countryId").equalTo(countryId)
       .on("value", snapshot => {
@@ -140,37 +141,37 @@ exports.handleResponsePlans = functions.database.ref('/sand/responsePlan/{countr
         admin.database().ref('/sand/countryOffice/' + agencyId + '/' + countryId + '/clockSettings/responsePlans')
           .on('value', snapshot => {
             console.log(snapshot.val());
-            let durationType = snapshot.val()['durationType'];
-            let value = snapshot.val()['value'];
+            let durationType = Number(snapshot.val()['durationType']);
+            let value = Number(snapshot.val()['value']);
+            let oneDayTime = 24 * 60 * 60 * 1000;
+            let timeDifference = 0;
+            switch (durationType) {
+              case WEEK:
+                timeDifference = value * 7 * oneDayTime;
+                break;
+              case MONTH:
+                timeDifference = value * 30 * oneDayTime; // one month has 30 days
+                break;
+              case YEAR:
+                timeDifference = value * 365 * oneDayTime; // one year has 365 days
+                break;
+            }
+            console.log(timeDifference);
+
+            setTimeout(() => {
+              admin.database().ref('/sand/responsePlan/' + countryId + '/' + responsePlanId)
+                .on('value', snapshot => {
+                  if (snapshot.val() && snapshot.val()['isActive']) {
+                    admin.database().ref('/sand/responsePlan/' + countryId + '/' + responsePlanId + '/isActive').set(false).then(() => {
+                      console.log("expire success");
+                    }, error => {
+                      console.log(error.message);
+                    });
+                  }
+                });
+            }, 5000);
+
           })
-
       });
-
-    // admin.database().ref('/sand/countryOffice/')
-    //   .on("value", snapshot => {
-    //     console.log("******");
-    //     console.log(snapshot.val());
-    //   });
-
-    // functions.database.ref('/sand/')
-    // const userId = event.params.userId;
-    // const preData = event.data.previous.val();
-    // const currData = event.data.current.val();
-    // console.log(preData);
-    // console.log(currData);
-    // if (!preData && currData) {
-    //   //add user account
-    //   console.log("user added: " + userId);
-    // } else if (preData && currData) {
-    //   //user account change
-    //   console.log("user data changed: " + userId);
-    // } else if (preData && !currData) {
-    //   //delete user account
-    //   console.log("delete user: " + userId);
-    //   // admin.auth().deleteUser(userId).then(() => {
-    //   //   console.log("successfully deleted user: " + userId);
-    //   // }, error => {
-    //   //   console.log(error.message);
-    //   // });
-    // }
   });
+
