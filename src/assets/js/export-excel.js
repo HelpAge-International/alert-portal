ffcc59621271dc1ff6eb690748e85a19b70344e5
Gishan Don Ranasinghe <dongishan@users-MacBook-Air.local>
2017-06-19 15:06:45 +0100

    Minor updates

--- src/assets/js/export-excel.js
+++ src/assets/js/export-excel.js
@@ -4,7 +4,7 @@
 var exportToExcelFile = (function () {
   var uri = 'data:application/vnd.ms-excel;base64,'
     ,
-    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta http-equiv=Content-Type content="text/html; charset=windows-1252"><meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"/></head><body><table>{table}</table></body></html>' +
+    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv=Content-Type content="text/html; charset=windows-1252"> <meta name=ProgId content=Excel.Sheet> <meta name=Generator content="Microsoft Excel 11"/></head><body><table>{table}</table></body></html>' +
       '</head><body><table>{table}</table></body></html>'
     , base64 = function (s) {
       return window.btoa(unescape(encodeURIComponent(s)))
