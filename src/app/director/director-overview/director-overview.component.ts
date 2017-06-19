3ed290db25c240968a63d8fe8b5be22648c46730
Fei Xie <fei@rolleragency.co.uk>
2017-06-19 14:40:06 +0100

    Overview wip

--- src/app/director/director-overview/director-overview.component.ts
+++ src/app/director/director-overview/director-overview.component.ts
@@ -1,21 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {Subject} from "rxjs/Subject";
 
 @Component({
   selector: 'app-director-overview',
   templateUrl: './director-overview.component.html',
   styleUrls: ['./director-overview.component.css']
 })
-export class DirectorOverviewComponent implements OnInit {
+export class DirectorOverviewComponent implements OnInit, OnDestroy {
+
+
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   private menuTab = "officeProfile";
+  private tabMap = new Map<string, boolean>();
 
-  constructor() { }
+  private countryId: string;
+  private isViewing: boolean;
+
+  constructor(private route: ActivatedRoute) {
+    this.tabMap.set("officeProfile", true);
+    this.tabMap.set("risk", false);
+    this.tabMap.set("preparedness", false);
+    this.tabMap.set("plan", false);
+  }
 
   ngOnInit() {
+    this.route.params
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe((params: Params) => {
+        if (params["countryId"]) {
+          this.countryId = params["countryId"];
+        }
+
+        if (params["isViewing"]) {
+          this.isViewing = params["isViewing"];
+        }
+      });
   }
 
-  menuSelection(menuName:string) {
-    console.log(menuName);
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
+  menuSelection(menuName: string) {
+    this.tabMap.forEach((v, k) => {
+      this.tabMap.set(k, k == menuName);
+    });
+  }
 }
