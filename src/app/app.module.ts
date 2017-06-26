4a49e2d102ed5857e446f4c9e3f0cbff9800fcc3
Fei Xie <fei@rolleragency.co.uk>
2017-06-26 16:49:29 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -352,6 +352,7 @@ const firebaseAuthConfig = {
     TooltipComponent,
     GuidanceReportComponent,
     TimeAgoPipe,
+    KeysPipe,
     CountryNotificationsComponent,
     ProjectActivityReportComponent,
     ReportingDatasheetComponent,
