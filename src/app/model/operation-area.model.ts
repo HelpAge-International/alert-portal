743ef041d135f796ad0ab141c1a7faa6e8c4495b
Krešimir Pleše <kresimir.plese@appscometrue.com>
2017-06-30 13:35:24 +0200

    Allowed selecting country only when creating new alert - ALT-1566

--- src/app/model/operation-area.model.ts
+++ src/app/model/operation-area.model.ts
@@ -10,12 +10,13 @@ export class OperationAreaModel extends BaseModel {
         if (typeof (this.country) == 'undefined' && !this.isExcluded('country', excludedFields)) {
             return new AlertMessageModel('ADD_PARTNER.NO_OPERATION_AREA_COUNTRY');
         }
-        if (typeof (this.level1) == 'undefined' && !this.isExcluded('level1', excludedFields)) {
+        //Commented out because one should be able to select country only
+        /*if (typeof (this.level1) == 'undefined' && !this.isExcluded('level1', excludedFields)) {
             return new AlertMessageModel('ADD_PARTNER.NO_OPERATION_AREA_LEVEL1');
         }
         if (typeof (this.level2) == 'undefined' && !this.isExcluded('level2', excludedFields)) {
             return new AlertMessageModel('ADD_PARTNER.NO_OPERATION_AREA_LEVEL2');
-        }
+        }*/
         return null;
     }
 
