f8984aebdf208fbdd2e5d1ff4844b12324b758ed
Fei Xie <fei@rolleragency.co.uk>
2017-07-25 12:17:44 +0100

    Fixed ALT-1839

--- src/app/risk-monitoring/risk-monitoring.component.ts
+++ src/app/risk-monitoring/risk-monitoring.component.ts
@@ -10,11 +10,10 @@ import {TranslateService} from "@ngx-translate/core";
 import {UserService} from "../services/user.service";
 import {Subject} from "rxjs/Subject";
 import {CountryPermissionsMatrix, PageControlService} from "../services/pagecontrol.service";
-import * as moment from "moment";
-import _date = moment.unitOfTime._date;
 
 
 declare var jQuery: any;
+
 @Component({
   selector: 'app-risk-monitoring',
   templateUrl: './risk-monitoring.component.html',
@@ -39,7 +38,6 @@ export class RiskMonitoringComponent implements OnInit, OnDestroy {
   private canCopy: boolean;
   private agencyOverview: boolean;
 
-  private agencyAdminId: string;
   private countryLocation: any;
   private Countries = Countries;
 
@@ -136,71 +134,25 @@ export class RiskMonitoringComponent implements OnInit, OnDestroy {
         }
 
         if (this.agencyId && this.countryID) {
-          this._getHazards()
+          this._getHazards();
           this.getCountryLocation();
           this._getCountryContextIndicators();
         } else {
           this.pageControl.authUserObj(this.ngUnsubscribe, this.route, this.router, (user, userType, countryId, agencyId, systemId) => {
             this.uid = user.uid;
             this.UserType = userType;
-
-            // if (this.agencyId && this.countryID) {
-            //   this._getHazards()
-            //   this._getCountryContextIndicators();
-            // } else {
             this.agencyId = agencyId;
             this.countryID = countryId;
             this.systemId = systemId;
             this._getHazards();
             this.getCountryLocation();
             this._getCountryContextIndicators();
-            // this._getCountryID().then(() => {
-            //   this.getAgencyID().then(() => {
-            //     this.getCountryLocation();
-            //   });
-            //   this._getHazards().then(() => {
-            //
-            //   });
-            //   this._getCountryContextIndicators();
-            // });
-            // }
-
             PageControlService.countryPermissionsMatrix(this.af, this.ngUnsubscribe, this.uid, userType, (isEnabled => {
               this.countryPermissionsMatrix = isEnabled;
             }));
           });
         }
-
-
-        // this.pageControl.auth(this.ngUnsubscribe, this.route, this.router, (user, userType) => {
-        //   this.uid = user.uid;
-        //   this.UserType = userType;
-        //
-        //   if (this.agencyId && this.countryID) {
-        //     this._getHazards().then(() => {
-        //
-        //     });
-        //     this._getCountryContextIndicators();
-        //   } else {
-        //     this._getCountryID().then(() => {
-        //       this.getAgencyID().then(() => {
-        //         this.getCountryLocation();
-        //       });
-        //       this._getHazards().then(() => {
-        //
-        //       });
-        //       this._getCountryContextIndicators();
-        //     });
-        //   }
-        //
-        //   PageControlService.countryPermissionsMatrix(this.af, this.ngUnsubscribe, this.uid, userType, (isEnabled => {
-        //     this.countryPermissionsMatrix = isEnabled;
-        //   }));
-        //
-        // });
       })
-
-
   }
 
   ngOnDestroy(): void {
@@ -208,31 +160,9 @@ export class RiskMonitoringComponent implements OnInit, OnDestroy {
     this.ngUnsubscribe.complete();
   }
 
-  _getCountryID() {
-    let promise = new Promise((res, rej) => {
-      this.af.database.object(Constants.APP_STATUS + "/" + Constants.USER_PATHS[this.UserType] + "/" + this.uid + '/countryId').takeUntil(this.ngUnsubscribe).subscribe((countryID: any) => {
-        this.countryID = countryID.$value ? countryID.$value : "";
-        res(true);
-      });
-    });
-    return promise;
-  }
-
-  private getAgencyID() {
-    let promise = new Promise((res, rej) => {
-      this.af.database.list(Constants.APP_STATUS + "/" + Constants.USER_PATHS[this.UserType] + "/" + this.uid + '/agencyAdmin')
-        .takeUntil(this.ngUnsubscribe)
-        .subscribe((agencyIds: any) => {
-          this.agencyAdminId = agencyIds[0].$key ? agencyIds[0].$key : "";
-          res(true);
-        });
-    });
-    return promise;
-  }
-
   private getCountryLocation() {
     let promise = new Promise((res, rej) => {
-      this.af.database.object(Constants.APP_STATUS + "/countryOffice/" + this.agencyAdminId + '/' + this.countryID + "/location")
+      this.af.database.object(Constants.APP_STATUS + "/countryOffice/" + this.agencyId + '/' + this.countryID + "/location")
         .takeUntil(this.ngUnsubscribe)
         .subscribe((location: any) => {
           this.countryLocation = location.$value ? location.$value : 0;
