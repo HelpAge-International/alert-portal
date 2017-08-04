8738b01d67b2c6a0ac776da3c3459ce1bb52b5b4
Jordan Fisher <jordan@rolleragency.co.uk>
2017-08-04 08:10:39 +0100

    Fixed misalignment of INFORM scanning with constants

--- src/app/utils/Enums.ts
+++ src/app/utils/Enums.ts
@@ -1356,9 +1356,9 @@ export class InformCodes {
     informCodes.map.set("HA.NAT.EQ", HazardScenario.HazardScenario4); // Earthquake
     informCodes.map.set("HA.NAT.TS", HazardScenario.HazardScenario24); // Tsunami
     informCodes.map.set("HA.NAT.DR", HazardScenario.HazardScenario3); // Drought
-    informCodes.map.set("HA.HUM.CON.GCRI", HazardScenario.HazardScenario2); // Conflict
+    informCodes.map.set("HA.HUM.CON.GCRI", HazardScenario.HazardScenario1); // Conflict
     informCodes.map.set("VU.SEV.AD.FTS-POP", HazardScenario.HazardScenario11); // Humanitarian Access
-    informCodes.map.set("HA.NAT.TC.CS", HazardScenario.HazardScenario11); // Storm Surge
+    informCodes.map.set("HA.NAT.TC.CS", HazardScenario.HazardScenario21); // Storm Surge
     informCodes.list = [];
     informCodes.map.forEach((val, key) => {
       informCodes.list.push(key);
