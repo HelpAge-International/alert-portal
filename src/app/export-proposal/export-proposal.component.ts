fbde654a61be288721c2dd354a6e795aff256438
Fei Xie <fei@rolleragency.co.uk>
2017-08-25 15:20:51 +0100

    fixed ALT-1951

--- src/app/export-proposal/export-proposal.component.ts
+++ src/app/export-proposal/export-proposal.component.ts
@@ -7,9 +7,11 @@ import {ResponsePlan} from "../model/responsePlan";
 import {UserService} from "../services/user.service";
 import {Constants} from "../utils/Constants";
 import {
+  BudgetCategory,
   HazardScenario,
   MediaFormat, MethodOfImplementation, PresenceInTheCountry, ResponsePlanSectors, SourcePlan, UserType
 } from "../utils/Enums";
+import {ModelPlanActivity} from "../model/plan-activity.model";
 
 @Component({
   selector: 'app-export-proposal',
@@ -49,7 +51,30 @@ export class ExportProposalComponent implements OnInit, OnDestroy {
   private systemAdminUid: string;
   private isExcel: number;
   private docType: string;
-
+  private totalInputs: number;
+  private totalOfAllCosts: number;
+  private total: number;
+  private transportBudget: number;
+  private transportNarrative: string;
+  private securityBudget: number;
+  private securityNarrative: string;
+  private logisticsAndOverheadsBudget: number;
+  private logisticsAndOverheadsNarrative: string;
+  private staffingAndSupportBudget: number;
+  private staffingAndSupportNarrative: string;
+  private monitoringAndEvolutionBudget: number;
+  private monitoringAndEvolutionNarrative: string;
+  private capitalItemsBudget: number;
+  private capitalItemsNarrative: string;
+  private managementSupportNarrative: string;
+  private activityMap = new Map();
+  private sectors: any[];
+  private totalFemaleUnder18 : number;
+  private totalFemale18To50 : number;
+  private totalFemaleOver50 : number;
+  private totalMaleUnder18 : number;
+  private totalMale18To50 : number;
+  private totalMaleOver50 : number;
   constructor(private pageControl: PageControlService, private af: AngularFire, private router: Router, private userService: UserService, private route: ActivatedRoute) {
   }
 
@@ -138,7 +163,8 @@ export class ExportProposalComponent implements OnInit, OnDestroy {
         this.bindProjectLeadData(responsePlan);
         this.bindPartnersData(responsePlan);
         this.bindSourcePlanData(responsePlan);
-
+        this.bindProjectActivitiesData(responsePlan);
+        this.bindProjectBudgetData(responsePlan);
       });
   }
 
@@ -209,10 +235,195 @@ export class ExportProposalComponent implements OnInit, OnDestroy {
       });
   }
 
+  private bindProjectBudgetData(responsePlan: ResponsePlan) {
+    if (responsePlan.budget) {
+      this.totalInputs = responsePlan.budget['totalInputs'] ? responsePlan.budget['totalInputs'] : 0;
+      this.totalOfAllCosts = responsePlan.budget['totalOfAllCosts'] ? responsePlan.budget['totalOfAllCosts'] : 0;
+      this.total = responsePlan.budget['total'] ? responsePlan.budget['total'] : 0;
+
+      if (responsePlan.budget['item']) {
+        this.transportBudget = responsePlan.budget['item'][BudgetCategory.Transport] ? responsePlan.budget['item'][BudgetCategory.Transport]['budget'] : 0;
+        this.transportNarrative = responsePlan.budget['item'][BudgetCategory.Transport] ? responsePlan.budget['item'][BudgetCategory.Transport]['narrative'] : '';
+
+        this.securityBudget = responsePlan.budget['item'][BudgetCategory.Security] ? responsePlan.budget['item'][BudgetCategory.Security]['budget'] : 0;
+        this.securityNarrative = responsePlan.budget['item'][BudgetCategory.Security] ? responsePlan.budget['item'][BudgetCategory.Security]['narrative'] : '';
+
+        this.logisticsAndOverheadsBudget = responsePlan.budget['item'][BudgetCategory.Logistics] ? responsePlan.budget['item'][BudgetCategory.Logistics]['budget'] : 0;
+        this.logisticsAndOverheadsNarrative = responsePlan.budget['item'][BudgetCategory.Logistics] ? responsePlan.budget['item'][BudgetCategory.Logistics]['narrative'] : '';
+
+        this.staffingAndSupportBudget = responsePlan.budget['item'][BudgetCategory.Staffing] ? responsePlan.budget['item'][BudgetCategory.Staffing]['budget'] : 0;
+        this.staffingAndSupportNarrative = responsePlan.budget['item'][BudgetCategory.Staffing] ? responsePlan.budget['item'][BudgetCategory.Staffing]['narrative'] : '';
+
+        this.monitoringAndEvolutionBudget = responsePlan.budget['item'][BudgetCategory.Monitoring] ? responsePlan.budget['item'][BudgetCategory.Monitoring]['budget'] : 0;
+        this.monitoringAndEvolutionNarrative = responsePlan.budget['item'][BudgetCategory.Monitoring] ? responsePlan.budget['item'][BudgetCategory.Monitoring]['narrative'] : '';
+
+        this.capitalItemsBudget = responsePlan.budget['item'][BudgetCategory.CapitalItems] ? responsePlan.budget['item'][BudgetCategory.CapitalItems]['budget'] : 0;
+        this.capitalItemsNarrative = responsePlan.budget['item'][BudgetCategory.CapitalItems] ? responsePlan.budget['item'][BudgetCategory.CapitalItems]['narrative'] : '';
+
+        this.managementSupportNarrative = responsePlan.budget['item'][BudgetCategory.ManagementSupport] ? responsePlan.budget['item'][BudgetCategory.ManagementSupport]['narrative'] : '';
+      }
+    }
+  }
+
+
+  private bindProjectActivitiesData(responsePlan: ResponsePlan){
+    if (responsePlan.sectors) {
+      this.sectors = Object.keys(responsePlan.sectors).map(key => {
+        let sector = responsePlan.sectors[key];
+        sector["id"] = Number(key);
+        return sector;
+      });
+    }
+
+    if (this.sectors) {
+      Object.keys(responsePlan.sectors).forEach(sectorKey => {
+
+        let activitiesData: {} = responsePlan.sectors[sectorKey]["activities"];
+        if(activitiesData)
+        {
+          let moreData: {}[] = [];
+          Object.keys(activitiesData).forEach(key => {
+            let beneficiary = [];
+            activitiesData[key]["beneficiary"].forEach(item => {
+              beneficiary.push(item);
+            });
+            let model = new ModelPlanActivity(activitiesData[key]["name"], activitiesData[key]["output"], activitiesData[key]["indicator"], beneficiary);
+            moreData.push(model);
+            this.activityMap.set(Number(sectorKey), moreData);
+          });
+        }
+      });
+
+      if (this.activityMap) {
+        this.totalFemaleUnder18 = this.getTotalFemaleUnder18(this.activityMap);
+        this.totalFemale18To50 = this.getTotalFemaleUnder18To50(this.activityMap);
+        this.totalFemaleOver50 = this.getTotalFemaleOver50(this.activityMap);
+
+        this.totalMaleUnder18 = this.getTotalMaleUnder18(this.activityMap);
+        this.totalMale18To50 = this.getTotalMaleUnder18To50(this.activityMap);
+        this.totalMaleOver50 = this.getTotalMaleOver50(this.activityMap);
+      }
+    }
+  }
   /**
    * Utility Functions
    */
 
+  private getTotalFemaleUnder18(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[0] ? Number(activity.beneficiary[0]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getTotalFemaleUnder18To50(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[1] ? Number(activity.beneficiary[1]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getTotalFemaleOver50(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[2] ? Number(activity.beneficiary[2]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getTotalMaleUnder18(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[3] ? Number(activity.beneficiary[3]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getTotalMaleUnder18To50(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[4] ? Number(activity.beneficiary[4]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getTotalMaleOver50(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[5] ? Number(activity.beneficiary[5]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getOverallFemaleTotal(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[0] ? Number(activity.beneficiary[0]["value"]) : 0)
+            + (activity.beneficiary && activity.beneficiary[1] ? Number(activity.beneficiary[1]["value"]) : 0)
+            + (activity.beneficiary && activity.beneficiary[2] ? Number(activity.beneficiary[2]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
+
+  private getOverallMaleTotal(activityMap){
+    var total = 0;
+    this.sectors.forEach(function (sector){
+      if(activityMap.get(sector.id))
+      {
+        activityMap.get(sector.id).forEach(function(activity) {
+          total += (activity.beneficiary && activity.beneficiary[3] ? Number(activity.beneficiary[3]["value"]) : 0)
+            +(activity.beneficiary && activity.beneficiary[4] ? Number(activity.beneficiary[4]["value"]) : 0)
+            +(activity.beneficiary && activity.beneficiary[5] ? Number(activity.beneficiary[5]["value"]) : 0);
+        });
+      }
+    });
+
+    return total;
+  }
   private downloadGroups(responsePlan: ResponsePlan) {
     if (this.systemAdminUid) {
       this.af.database.list(Constants.APP_STATUS + "/system/" + this.systemAdminUid + '/groups')
