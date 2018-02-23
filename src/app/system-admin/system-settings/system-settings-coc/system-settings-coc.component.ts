1c57d16c6444b8815f8a73ee6789e867a61adc55
Gishan Don Ranasinghe <gd416@cam.ac.uk>
2018-02-23 11:45:08 +0000

    Functionality implemented for system admin to update CoC

--- src/app/system-admin/system-settings/system-settings-coc/system-settings-coc.component.ts
+++ src/app/system-admin/system-settings/system-settings-coc/system-settings-coc.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Constants} from "../../../utils/Constants";
+import {Subject} from "rxjs/Subject";
+import {PageControlService} from "../../../services/pagecontrol.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AngularFire} from "angularfire2";
 
 @Component({
   selector: 'app-system-settings-coc',
@@ -9,10 +13,31 @@ import {Constants} from "../../../utils/Constants";
 export class SystemSettingsCocComponent implements OnInit {
 
   private isEditing: boolean = false;
+  private cocText: string = "";
+  private uid: string;
 
-  constructor() { }
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
+  constructor(private pageControl: PageControlService, private route: ActivatedRoute, private af: AngularFire, private router: Router) {
+  }
 
   ngOnInit() {
+    this.pageControl.auth(this.ngUnsubscribe, this.route, this.router, (user, userType) => {
+      this.uid = user.uid;
+      this.downloadCoC();
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  private downloadCoC(){
+    this.af.database.object(Constants.APP_STATUS + "/system/" + this.uid)
+      .takeUntil(this.ngUnsubscribe).subscribe(x => {
+      this.cocText = x.coc;
+    });
   }
 
   edit(event) {
@@ -20,12 +45,11 @@ export class SystemSettingsCocComponent implements OnInit {
   }
 
   cancelEdit(event) {
-    //Cancel
     this.isEditing = false;
   }
 
   saveEdited() {
-    //Save
+    this.isEditing = false;
+    this.af.database.object(Constants.APP_STATUS + "/system/" + this.uid +"/coc").set(this.cocText);
   }
-
 }
