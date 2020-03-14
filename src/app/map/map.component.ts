bbf721ff263bf809ed4c740544c230fd5c54d46f
Faiz Mohideen <faiz@nuom.co.uk>
2020-03-14 15:12:49 +0000

    Global Map - Showing hazard marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -27,85 +27,73 @@ import { Constants } from "../utils/Constants";
 import { Countries, CountriesMapsSearchInterface } from "../utils/Enums";
 import { HazardImages } from "../utils/HazardImages";
 import { DepHolder, SDepHolder, SuperMapComponents } from "../utils/MapHelper";
-​
+
 import GeocoderResult = google.maps.GeocoderResult;
 import GeocoderStatus = google.maps.GeocoderStatus;
-​
-​
+
 declare var jQuery: any;
-​
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-​
+
 export class MapComponent implements OnInit, OnDestroy {
-​
-  private ngUnsubscribe: Subject<void> = new Subject<void>();
-​
   public uid: string;
   public mapHelper: SuperMapComponents;
-​
   public mapService: MapService;
-​
   public department: SDepHolder;
-  private countries: MapCountry[] = [];
-  private agencyMap: Map<string, string> = new Map<string, string>();
-​
   public agencyLogo: string;
-​
   public minThreshYellow: number;
   public minThreshGreen: number;
-​
+  public DEPARTMENT_MAP: Map<string, string> = new Map<string, string>();
+  public moduleAccess: AgencyModulesEnabled = new AgencyModulesEnabled();
+
   private isDirector: boolean;
   private userTypePath: string;
-  private geocoder: google.maps.Geocoder = new google.maps.Geocoder;
-​
-  public DEPARTMENT_MAP: Map<string, string> = new Map<string, string>();
-​
-  public moduleAccess: AgencyModulesEnabled = new AgencyModulesEnabled();
-  private map;
-  private vector: VectorLayer = new VectorLayer()
   private hazardExist = false
-​
-  constructor(private pageControl: PageControlService,
-              private af: AngularFire,
-              private router: Router,
-              private route: ActivatedRoute,
-              private userService: UserService,
-              private settingService: SettingsService,
-              private agencyService:AgencyService,
-              private translate: TranslateService) {
-    this.mapHelper = SuperMapComponents.init(af, this.ngUnsubscribe);
+  private countries: MapCountry[] = [];
+  private vector: VectorLayer = new VectorLayer()
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+  private agencyMap: Map<string, string> = new Map<string, string>();
+  private geocoder: google.maps.Geocoder = new google.maps.Geocoder;
 
-    
+  constructor(private pageControl: PageControlService,
+    private af: AngularFire,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private settingService: SettingsService,
+    private agencyService: AgencyService,
+    private translate: TranslateService) {
+    this.mapHelper = SuperMapComponents.init(af, this.ngUnsubscribe);
   }
-​
+
   goToListView() {
-    this.isDirector ? this.router.navigate(['map/map-countries-list', {'isDirector': true}]) : this.router.navigateByUrl('map/map-countries-list');
+    this.isDirector ? this.router.navigate(['map/map-countries-list', { 'isDirector': true }]) : this.router.navigateByUrl('map/map-countries-list');
   }
-​
+
   ngOnInit() {
     this.loadMapData()
   }
-​
+
   loadMapData() {
     this.department = new SDepHolder("Something");
     this.department.location = -1;
     this.department.departments.push(new DepHolder("Loading", 100, 1));
-​
-   this.route.params
+
+    this.route.params
       .takeUntil(this.ngUnsubscribe)
       .subscribe((params: Params) => {
         if (params["isDirector"]) {
           this.isDirector = params["isDirector"];
         }
-​
-       this.pageControl.authUser(this.ngUnsubscribe, this.route, this.router, (user, userType, countryId, agencyId, systemId) => {
+
+        this.pageControl.authUser(this.ngUnsubscribe, this.route, this.router, (user, userType, countryId, agencyId, systemId) => {
           this.uid = user.uid;
           this.userTypePath = Constants.USER_PATHS[userType];
-​
+
           this.mapService = MapService.init(this.af, this.ngUnsubscribe);
           this.mapService.init(this.uid, userType, countryId, agencyId, systemId,
             ((countries, green, yellow) => {
@@ -114,8 +102,8 @@ export class MapComponent implements OnInit, OnDestroy {
               this.minThreshYellow = yellow;
               this.setLocation(countries)
             }));
-​
-          this.af.database.list(Constants.APP_STATUS + "/agency/" + agencyId + "/departments", {preserveSnapshot: true})
+
+          this.af.database.list(Constants.APP_STATUS + "/agency/" + agencyId + "/departments", { preserveSnapshot: true })
             .takeUntil(this.ngUnsubscribe)
             .subscribe((snap) => {
               this.DEPARTMENT_MAP.clear();
@@ -123,8 +111,7 @@ export class MapComponent implements OnInit, OnDestroy {
               for (let x of snap) {
                 this.DEPARTMENT_MAP.set(x.key, x.val().name);
               }
-​
-              //try to add country local departments here
+
               if (this.isDirector) {
                 this.agencyService.getAllCountryIdsForAgency(agencyId)
                   .mergeMap(ids => Observable.from(ids))
@@ -139,7 +126,7 @@ export class MapComponent implements OnInit, OnDestroy {
                   })
               }
             });
-​
+
           /** Load in the markers on the map! */
           PageControlService.agencyQuickEnabledMatrix(this.af, this.ngUnsubscribe, this.uid, Constants.USER_PATHS[userType], isEnabled => {
             this.moduleAccess = isEnabled;
@@ -149,46 +136,40 @@ export class MapComponent implements OnInit, OnDestroy {
               });
             }
           });
-​
+
           /** Get the Agency logo */
           this.mapHelper.logoForAgencyAdmin(this.uid, Constants.USER_PATHS[userType], (logo) => {
             this.agencyLogo = logo;
           });
-
-
         });
       });
   }
 
   private setLocation(countries) {
     let position = 0;
-   
 
     var promise = new Promise((resolve, reject) => {
-       countries.forEach(country => {
-      let count: number = 0;
-      this.countries = countries
-      
-      if (country.hazards.size != 0) {
-        this.hazardExist = true
-        country.hazards.forEach((_, hazard) => {
-          console.log(country);
+      countries.forEach(country => {
+        let count: number = 0;
+        this.countries = countries
 
-          this.geocoder.geocode({ "address": CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(country.location) }, (geoResult: GeocoderResult[], status: GeocoderStatus) => {
-            if (status == GeocoderStatus.OK && geoResult.length >= 1) {
-              let pos = {
-                lng: geoResult[0].geometry.location.lng() + position,
-                lat: geoResult[0].geometry.location.lat()
-              };
-              console.log(hazard);
+        if (country.hazards.size != 0) {
+          this.hazardExist = true
+          country.hazards.forEach((_, hazard) => {
+            this.geocoder.geocode({ "address": CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(country.location) }, (geoResult: GeocoderResult[], status: GeocoderStatus) => {
+              if (status == GeocoderStatus.OK && geoResult.length >= 1) {
+                let pos = {
+                  lng: geoResult[0].geometry.location.lng() + position,
+                  lat: geoResult[0].geometry.location.lat()
+                };
 
                 var iconFeature = new Feature({
                   geometry: new Point(fromLonLat([pos.lng, pos.lat])),
-                  name: 'Null Island',
+                  name: '',
                   population: 4000,
                   rainfall: 500
                 });
-            
+
                 var iconStyle = new Style({
                   image: new Icon({
                     anchor: [0.5, 0.5],
@@ -198,95 +179,54 @@ export class MapComponent implements OnInit, OnDestroy {
                   })
                 });
                 iconFeature.setStyle(iconStyle);
-              
-              
-              this.vector = new VectorLayer({
-                source: new VectorSource({
-                  features: [iconFeature],
-                  format: new GeoJSON(),
-                  url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
-                }),
-                style: this.mapStyle()
-              });
-              
 
-              count++;
-              if (count % 2 == 0) {
-                position *= -1;
-              }
-              else {
-                position += 1.2;
-              }
+                this.vector = new VectorLayer({
+                  source: new VectorSource({
+                    features: [iconFeature],
+                    format: new GeoJSON(),
+                    url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
+                  }),
+                  style: this.mapStyle()
+                });
 
-              resolve();
-            }
+                count++;
+                if (count % 2 == 0) {
+                  position *= -1;
+                }
+                else {
+                  position += 1.2;
+                }
+
+                if (count == country.hazards.size) {
+                  resolve();
+                }
+              }
+            })
           })
-        })
-      }
+        }
+      })
     })
-  })
 
-  promise.then(() => {
-    this.initialiseMap(countries)
-  });
-
-    // if (hazardExist) {
-      
-    // } else {
-      // this.vector = new VectorLayer({
-      //   source: new VectorSource({
-      //     format: new GeoJSON(),
-      //     url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
-      //   }),
-      //   style: this.mapStyle()
-      // });
-
-      // this.initialiseMap(countries, hazardExist)
-    // }
-  }
-​
-  initialiseMap(countries) {
-    var raster = new Tile({
-      source: new OSM(),
+    promise.then(() => {
+      this.initialiseMap(countries)
     });
 
     if (!this.hazardExist) {
- this.vector = new VectorLayer({
+      this.vector = new VectorLayer({
         source: new VectorSource({
           format: new GeoJSON(),
           url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
         }),
         style: this.mapStyle()
       });
+      this.initialiseMap(countries)
     }
+  }
 
-    // if (hazard) {
-    //   var iconFeature = new Feature({
-    //     geometry: new Point(fromLonLat([position.lng, position.lat])),
-    //     name: 'Null Island',
-    //     population: 4000,
-    //     rainfall: 500
-    //   });
-  
-    //   var iconStyle = new Style({
-    //     image: new Icon({
-    //       anchor: [0.5, 0.5],
-    //       anchorXUnits: IconAnchorUnits.FRACTION,
-    //       anchorYUnits: IconAnchorUnits.FRACTION,
-    //       src: HazardImages.init().get(hazard)
-    //     })
-    //   });
-    //   iconFeature.setStyle(iconStyle);
-    // }
-    
-    // var vector = new VectorLayer({
-    //   source: new VectorSource({
-    //     features: iconFeature ? [iconFeature] : [],
-    //     format: new GeoJSON(),
-    //     url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
-    //   }),
-    //   style: this.mapStyle()
-    // });
+  initialiseMap(countries) {
+    var raster = new Tile({
+      source: new OSM(),
+    });
 
     var map = new OlMap({
       target: 'map',
@@ -312,7 +252,7 @@ export class MapComponent implements OnInit, OnDestroy {
 
       var info = document.getElementById('info');
       if (feature) {
-        info.innerHTML = feature.getId() + ': ' + feature.get('name');
+        info.innerHTML = feature.get('name');
       } else {
         info.innerHTML = '&nbsp;';
       }
@@ -350,7 +290,7 @@ export class MapComponent implements OnInit, OnDestroy {
       }
     });
   }
-​
+
   private mapStyle() {
     let countries = this.getAffectedCountries() as [Country]
     return function (feature, res) {
@@ -369,15 +309,15 @@ export class MapComponent implements OnInit, OnDestroy {
       }
     }
   }
-​
+
   private getAffectedCountries() {
     var countries = new Array()
-​
+
     for (let x of this.countries) {
-      var name = CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(x.location)      
+      var name = CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(x.location)
       let country: Country = new Country('', '')
       country.name = name
-​
+
       if (x.overall() == -1) {
         country.colour = 'blue'
       } else if (x.overall() >= this.minThreshGreen) {
@@ -391,27 +331,27 @@ export class MapComponent implements OnInit, OnDestroy {
     }
     return countries
   }
-​
+
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
   }
-​
+
   public getCountryCode(location: number) {
     return Countries[location];
   }
-​
+
   public openMinimumPreparednessModal(countryCode: string) {
     if (this.moduleAccess.minimumPreparedness) {
       jQuery("#minimum-prep-modal-" + countryCode).modal("show");
     }
   }
 }
-​
+
 export class Country {
   public name: String
   public colour: String
-​
+
   constructor(name: String, colour: String) {
     this.name = name
     this.colour = colour
