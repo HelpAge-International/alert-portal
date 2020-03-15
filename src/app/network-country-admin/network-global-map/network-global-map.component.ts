d3691acd320ab1997f445db76a680a83e2395c00
Faiz Mohideen <faiz@nuom.co.uk>
2020-03-15 08:36:17 +0000

    Global Map implemented on Network/Local Network

--- src/app/network-country-admin/network-global-map/network-global-map.component.ts
+++ src/app/network-country-admin/network-global-map/network-global-map.component.ts
@@ -1,15 +1,30 @@
-import {Component, OnInit, OnDestroy, Input} from '@angular/core';
-import {MapService} from '../../services/map.service';
-import {PageControlService} from '../../services/pagecontrol.service';
-import {AngularFire} from 'angularfire2';
-import {ActivatedRoute, Params, Router} from '@angular/router';
-import {UserService} from '../../services/user.service';
-import {TranslateService} from '@ngx-translate/core';
-import {Constants} from '../../utils/Constants';
-import {Subject} from 'rxjs/Subject';
-import {NetworkService} from '../../services/network.service';
-import {NetworkMapService} from '../../services/networkmap.service';
-import {Countries, NetworkUserAccountType} from "../../utils/Enums";
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { AngularFire } from 'angularfire2';
+import Feature from 'ol/Feature';
+import GeoJSON from 'ol/format/GeoJSON';
+import Point from 'ol/geom/Point';
+import Tile from "ol/layer/Tile";
+import VectorLayer from 'ol/layer/Vector';
+import OlMap from "ol/Map";
+import 'ol/ol.css';
+import { fromLonLat } from "ol/proj";
+import OSM from "ol/source/OSM";
+import VectorSource from 'ol/source/Vector';
+import { Fill, Stroke, Style } from 'ol/style';
+import Icon from 'ol/style/Icon';
+import IconAnchorUnits from "ol/style/IconAnchorUnits";
+import View from "ol/View";
+import { Subject } from 'rxjs/Subject';
+import { NetworkService } from '../../services/network.service';
+import { NetworkMapCountry, NetworkMapService } from '../../services/networkmap.service';
+import { PageControlService } from '../../services/pagecontrol.service';
+import { Constants } from '../../utils/Constants';
+import { Countries, CountriesMapsSearchInterface } from "../../utils/Enums";
+import { HazardImages } from '../../utils/HazardImages';
+
+import GeocoderResult = google.maps.GeocoderResult;
+import GeocoderStatus = google.maps.GeocoderStatus;
 
 declare var jQuery: any;
 
@@ -20,21 +35,25 @@ declare var jQuery: any;
 })
 
 export class NetworkGlobalMapComponent implements OnInit, OnDestroy {
-
-  private ngUnsubscribe: Subject<void> = new Subject<void>();
+  @Input() isLocalNetworkAdmin: boolean;
 
   public isViewing: boolean;
-  @Input() isLocalNetworkAdmin: boolean;
-  private paramString: string;
-
   public uid: string;
   public networkId: string;
   public networkCountryId: string;
   public systemAdminId: string;
 
-  private HazardScenario = Constants.HAZARD_SCENARIOS;
+  private hazardExist = false;
+  private paramString: string;
   private agencyId: string;
   private countryId: string;
+  private minYellow: number;
+  private minGreen: number;
+  private countries: NetworkMapCountry[] = [];
+  private HazardScenario = Constants.HAZARD_SCENARIOS;
+  private vector: VectorLayer = new VectorLayer()
+  private geocoder: google.maps.Geocoder = new google.maps.Geocoder;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(private pageControl: PageControlService,
               private af: AngularFire,
@@ -77,17 +96,19 @@ export class NetworkGlobalMapComponent implements OnInit, OnDestroy {
           if (params["isViewing"]) {
             this.isViewing = params["isViewing"];
           }
-          console.log("Network Country Id: " + this.networkCountryId);
           if (this.networkId != null && this.uid != null && this.systemAdminId) {
-            this.networkMapService.init('global-map', this.af, this.ngUnsubscribe, this.systemAdminId, this.networkId, this.networkCountryId, () => {
-              console.log("Network map initialised (viewing");
+            this.networkMapService.init(this.af, this.ngUnsubscribe, this.systemAdminId, this.networkId, this.networkCountryId, 
+            (countries, minGreen, minYellow) => {              
+              this.countries = countries;
+              this.minGreen = minGreen;
+              this.minYellow = minYellow;
+              this.setLocation(countries)
             }, (country) => {
               this.showDialog(country);
             });
           }
           else {
             this.uid = user.uid;
-            console.log(this.uid);
             this.networkService.getSelectedIdObj(this.uid)
               .takeUntil(this.ngUnsubscribe)
               .subscribe(selection => {
@@ -97,16 +118,14 @@ export class NetworkGlobalMapComponent implements OnInit, OnDestroy {
 
                 // TODO: Delete this method when page control does auth properly
 
-                console.log("Network Country Id: " + this.networkCountryId);
-
                 this.getSystemAdmin(this.uid, (systemAdminId => {
-
-                  this.networkMapService.init('global-map', this.af, this.ngUnsubscribe, systemAdminId, this.networkId, this.networkCountryId,
-                    () => {
-                      // THIS METHOD CALLED WHEN EVERYTHING IS DONE!!
-                      console.log("Network map initialised");
-                    },
-                    (country) => {
+                  this.networkMapService.init(this.af, this.ngUnsubscribe, systemAdminId, this.networkId, this.networkCountryId,
+                    (countries, minGreen, minYellow) => {              
+                      this.countries = countries;
+                      this.minGreen = minGreen;
+                      this.minYellow = minYellow;
+                      this.setLocation(countries)
+                    }, (country) => {
                       this.showDialog(country);
                     });
                 }));
@@ -114,6 +133,193 @@ export class NetworkGlobalMapComponent implements OnInit, OnDestroy {
           }
         });
     });
+  }
+
+  private setLocation(countries) {
+    let position = 0;
+
+    var promise = new Promise((resolve, reject) => {
+      countries.forEach(country => {
+        let count: number = 0;
+
+        if (country.hazards.length > 0) {
+          this.hazardExist = true
+
+            for(let hazard of country.hazards) {
+            this.geocoder.geocode({ "address": CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(country.location) }, (geoResult: GeocoderResult[], status: GeocoderStatus) => {
+              if (status == GeocoderStatus.OK && geoResult.length >= 1) {
+                let pos = {
+                  lng: geoResult[0].geometry.location.lng() + position,
+                  lat: geoResult[0].geometry.location.lat()
+                };
+                
+                var iconFeature = new Feature({
+                  geometry: new Point(fromLonLat([pos.lng, pos.lat])),
+                  name: '',
+                  population: 4000,
+                  rainfall: 500
+                });
+
+                var iconStyle = new Style({
+                  image: new Icon({
+                    anchor: [0.5, 0.5],
+                    anchorXUnits: IconAnchorUnits.FRACTION,
+                    anchorYUnits: IconAnchorUnits.FRACTION,
+                    src: HazardImages.init().get(hazard.hazardScenario)
+                  })
+                });
+                iconFeature.setStyle(iconStyle);
+
+                this.vector = new VectorLayer({
+                  source: new VectorSource({
+                    features: [iconFeature],
+                    format: new GeoJSON(),
+                    url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
+                  }),
+                  style: this.mapStyle()
+                });
+
+                count++;
+                if (count % 2 == 0) {
+                  position *= -1;
+                }
+                else {
+                  position += 1.2;
+                }
+
+                if (count == country.hazards.length) {
+                  resolve();
+                }
+              }
+            })
+          }
+        }
+      })
+    })
+
+    promise.then(() => {
+      this.initialiseMap(countries)
+    });
+
+    if (!this.hazardExist) {
+      this.vector = new VectorLayer({
+        source: new VectorSource({
+          format: new GeoJSON(),
+          url: 'https://raw.githubusercontent.com/openlayers/ol3/6838fdd4c94fe80f1a3c98ca92f84cf1454e232a/examples/data/geojson/countries.geojson'
+        }),
+        style: this.mapStyle()
+      });
+      this.initialiseMap(countries)
+    }
+  }
+
+  initialiseMap(countries) {
+    var raster = new Tile({
+      source: new OSM(),
+    });
+
+    var map = new OlMap({
+      target: 'map',
+      layers: [raster, this.vector],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+
+    var featureOverlay = new VectorLayer({
+      source: new VectorSource(),
+      map: map,
+      style: this.mapStyle()
+    });
+
+    var highlight, feature;
+
+    var displayFeatureInfo = function (pixel) {
+      feature = map.forEachFeatureAtPixel(pixel, function (feature) {
+        return feature;
+      });
+
+      var info = document.getElementById('info');
+      if (feature) {
+        info.innerHTML = feature.get('name');
+      } else {
+        info.innerHTML = '&nbsp;';
+      }
+
+      if (feature !== highlight) {
+        if (highlight) {
+          featureOverlay.getSource().removeFeature(highlight);
+        }
+        if (feature) {
+          featureOverlay.getSource().addFeature(feature);
+        }
+        highlight = feature;
+      }
+    };
+
+    map.on('pointermove', function (evt) {
+      if (evt.dragging) {
+        return;
+      }
+      var pixel = map.getEventPixel(evt.originalEvent);
+      displayFeatureInfo(pixel);
+    });
+
+    map.on('click', function (evt) {
+      if (feature != undefined) {
+        displayFeatureInfo(evt.pixel)
+
+        for (let x of countries) {
+          var name = CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(x.location)
+
+          if (feature.get("name") == name) {
+            jQuery('#minimum-prep-modal-' + Countries[x.location]).modal('show');
+          }
+        }
+      }
+    });
+  }
+
+  private mapStyle() {
+    let countries = this.getAffectedCountries() as [Country]
+    return function (feature, res) {
+      for (var country of countries) {
+        if (feature.get("name") == country.name) {
+          return new Style({
+            stroke: new Stroke({
+              color: String(country.colour),
+              width: 2
+            }),
+            fill: new Fill({
+              color: String(country.colour)
+            })
+          });
+        }
+      }
+    }
+  }
+
+  private getAffectedCountries() {
+    var countries = new Array()
+
+    for (let x of this.countries) {
+      var name = CountriesMapsSearchInterface.getEnglishLocationFromEnumValue(x.location)
+      let country: Country = new Country('', '')
+      country.name = name
+
+      if (x.overall(this.minGreen) == -1) {
+        country.colour = 'blue'
+      } else if (x.overall(this.minGreen) >= this.minGreen) {
+        country.colour = 'red'
+      } else if (x.overall(this.minGreen) >= this.minYellow) {
+        country.colour = 'yellow'
+      } else {
+        country.colour = 'red'
+      }
+      countries.push(country)
+    }
+    return countries
   }
 
   ngOnDestroy() {
@@ -171,12 +377,17 @@ export class NetworkGlobalMapComponent implements OnInit, OnDestroy {
       });
   }
 
-
-
-  /**
-   * Show the popup dialog
-   */
   public showDialog(countryCode: string) {
     jQuery('#minimum-prep-modal-' + countryCode).modal('show');
+  }
+}
+
+export class Country {
+  public name: String
+  public colour: String
+
+  constructor(name: String, colour: String) {
+    this.name = name
+    this.colour = colour
   }
 }
